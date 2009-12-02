7f7d63d2aa2d8f3b322df9a0cf438659f1a8633c
Tony Garnock-Jones <tonyg@kcbbs.gen.nz>
2009-12-02 06:45:23 +0000

    Add convenience repl_channel function.

--- pika/__init__.py
+++ pika/__init__.py
@@ -12,3 +12,6 @@ asyncore_loop = pika.asyncore_adapter.loop
 
 from pika.blocking_adapter import \
     BlockingConnection
+
+def repl_channel(host = '127.0.0.1', *args):
+    return BlockingConnection(ConnectionParameters(host, *args)).channel()
