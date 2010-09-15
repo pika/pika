439b673f9a9ec9c14eb7b17fed00f7e2458a3323
Gavin M. Roy <gmr@myyearbook.com>
2010-09-15 15:27:15 -0400

    Minor code cleanup, remove unneeded function, make flush work, etc

--- pika/tornado_adapter.py
+++ pika/tornado_adapter.py
@@ -12,6 +12,7 @@ class TornadoConnection(pika.connection.AsyncConnection):
     product = "Pika/Tornado"
     
     def delayed_call(self, delay_sec, callback):
+    
         deadline = time.time() + delay_sec
         self.io_loop.add_timeout(deadline, callback)
 
@@ -23,38 +24,28 @@ class TornadoConnection(pika.connection.AsyncConnection):
         self.io_loop = tornado.ioloop.IOLoop.instance()
         self.handle_connection_open()
           
+        # Append our handler to tornado's ioloop for our socket
         events = tornado.ioloop.IOLoop.READ | tornado.ioloop.IOLoop.ERROR
         self.io_loop.add_handler(self.sock.fileno(), self._handle_events, events)
         
+        # Loop on handle_write every 0.1 second        
         self.delayed_call(0.1, self._handle_write)
-        
-  
-    def wait_for_open(self):
-        logging.debug("In wait for open")
-        while (not self.connection_open) and \
-            (self.reconnection_strategy.can_reconnect() or (not self.connection_close)):
-            self.drain_events()        
-    
         
     def _handle_events(self, fd, events):
         
+        # Incoming events from IOLoop, make sure we have our socket
         if not self.sock:
             logging.warning("Got events for closed stream %d", fd)
             return
+
         if events & tornado.ioloop.IOLoop.READ:
             self._handle_read()
-            
-        if not self.sock:
-            return
-        if events & tornado.ioloop.IOLoop.WRITE:
-            self._handle_write()
-        if not self.sock:
-            return
+
         if events & tornado.ioloop.IOLoop.ERROR:
             self.sock.close()
-            return
 
     def _handle_read(self):
+    
         try:
             chunk = self.sock.recv(self.suggested_buffer_size())
         except socket.error, e:
@@ -74,7 +65,8 @@ class TornadoConnection(pika.connection.AsyncConnection):
         # Let our parent class have the data
         self.on_data_available(chunk)
         
-    def _handle_write(self):
+    def _handle_write(self, add_timer = True):
+
         if len(self.outbound_buffer):
             fragment = self.outbound_buffer.read()
             try:
@@ -87,14 +79,20 @@ class TornadoConnection(pika.connection.AsyncConnection):
                                     self.sock.fileno(), e)
                     self.close()
                     return
-                    
+            
+            # Remove the content we used from our buffer
             self.outbound_buffer.consume(r)
-        self.delayed_call(0.1, self._handle_write)
-          
+            
+        # Loop on this ever 0.1 second
+        if add_time is True:
+            self.delayed_call(0.1, self._handle_write)
          
     def disconnect_transport(self):
+
+        # Close our socket since the Conneciton class told us to do so
         self.sock.close()
         
     def flush_outbound(self):
-        #select.select([self.sock], [self.sock], [self.sock])
-        pass
+    
+        # Lets just make sure we've written out our buffer but dont make a timer
+        self._handle_write(False)
