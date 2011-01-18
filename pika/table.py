740bab4df0792e3bfb26e7f3eb36a93fc49e5994
Gavin M. Roy <gmr@myyearbook.com>
2011-01-18 14:38:09 -0500

    PEP8 cleanup and python addition idiom change

--- pika/table.py
+++ pika/table.py
@@ -53,11 +53,12 @@ import calendar
 
 from pika.exceptions import *
 
+
 def encode_table(pieces, table):
     if table is None:
         table = {}
     length_index = len(pieces)
-    pieces.append(None) # placeholder
+    pieces.append(None)  # placeholder
     tablesize = 0
     for (key, value) in table.iteritems():
         pieces.append(struct.pack('B', len(key)))
@@ -81,48 +82,53 @@ def encode_table(pieces, table):
                 pieces.append(struct.pack('>cBI', 'D', 0, int(value)))
             tablesize = tablesize + 5
         elif isinstance(value, datetime.datetime):
-            pieces.append(struct.pack('>cQ', 'T', calendar.timegm(value.utctimetuple())))
+            pieces.append(struct.pack('>cQ', 'T',
+                          calendar.timegm(value.utctimetuple())))
             tablesize = tablesize + 9
         elif isinstance(value, dict):
             pieces.append(struct.pack('>c', 'F'))
             tablesize = tablesize + 1 + encode_table(pieces, value)
         else:
-            raise InvalidTableError("Unsupported field kind during encoding", key, value)
+            raise InvalidTableError("Unsupported field kind during encoding",
+                                    key, value)
     pieces[length_index] = struct.pack('>I', tablesize)
     return tablesize + 4
+
 
 def decode_table(encoded, offset):
     result = {}
     tablesize = struct.unpack_from('>I', encoded, offset)[0]
-    offset = offset + 4
+    offset += 4
     limit = offset + tablesize
     while offset < limit:
         keylen = struct.unpack_from('B', encoded, offset)[0]
-        offset = offset + 1
-        key = encoded[offset : offset + keylen]
-        offset = offset + keylen
+        offset += 1
+        key = encoded[offset: offset + keylen]
+        offset += keylen
         kind = encoded[offset]
-        offset = offset + 1
+        offset += 1
         if kind == 'S':
             length = struct.unpack_from('>I', encoded, offset)[0]
-            offset = offset + 4
-            value = encoded[offset : offset + length]
-            offset = offset + length
+            offset += 4
+            value = encoded[offset: offset + length]
+            offset += length
         elif kind == 'I':
             value = struct.unpack_from('>I', encoded, offset)[0]
-            offset = offset + 4
+            offset += 4
         elif kind == 'D':
             decimals = struct.unpack_from('B', encoded, offset)[0]
-            offset = offset + 1
+            offset += 1
             raw = struct.unpack_from('>I', encoded, offset)[0]
-            offset = offset + 4
+            offset += 4
             value = decimal.Decimal(raw) * (decimal.Decimal(10) ** -decimals)
         elif kind == 'T':
-            value = datetime.datetime.utcfromtimestamp(struct.unpack_from('>Q', encoded, offset)[0])
-            offset = offset + 8
+            value = datetime.datetime.utcfromtimestamp(struct.unpack_from('>Q',
+                                                       encoded, offset)[0])
+            offset += 8
         elif kind == 'F':
             (value, offset) = decode_table(encoded, offset)
         else:
-            raise InvalidTableError("Unsupported field kind %s during decoding" % (kind,))
+            raise InvalidTableError("Unsupported field kind %s while decoding"\
+                                    % (kind,))
         result[key] = value
     return (result, offset)
