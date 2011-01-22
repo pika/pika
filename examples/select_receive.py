fe7ba04db65ca41202dbfa54992ff953326e8038
Gavin M. Roy <gmr@myyearbook.com>
2011-01-22 18:24:33 -0500

    Demo for using the SelectConnection

---
+++ examples/select_receive.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1/GPL 2.0
+#
+# The contents of this file are subject to the Mozilla Public License
+# Version 1.1 (the "License"); you may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS"
+# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+# the License for the specific language governing rights and
+# limitations under the License.
+#
+# The Original Code is Pika.
+#
+# The Initial Developers of the Original Code are LShift Ltd, Cohesive
+# Financial Technologies LLC, and Rabbit Technologies Ltd.  Portions
+# created before 22-Nov-2008 00:00:00 GMT by LShift Ltd, Cohesive
+# Financial Technologies LLC, or Rabbit Technologies Ltd are Copyright
+# (C) 2007-2008 LShift Ltd, Cohesive Financial Technologies LLC, and
+# Rabbit Technologies Ltd.
+#
+# Portions created by LShift Ltd are Copyright (C) 2007-2009 LShift
+# Ltd. Portions created by Cohesive Financial Technologies LLC are
+# Copyright (C) 2007-2009 Cohesive Financial Technologies
+# LLC. Portions created by Rabbit Technologies Ltd are Copyright (C)
+# 2007-2009 Rabbit Technologies Ltd.
+#
+# Portions created by Tony Garnock-Jones are Copyright (C) 2009-2010
+# LShift Ltd and Tony Garnock-Jones.
+#
+# All Rights Reserved.
+#
+# Contributor(s): ______________________________________.
+#
+# Alternatively, the contents of this file may be used under the terms
+# of the GNU General Public License Version 2 or later (the "GPL"), in
+# which case the provisions of the GPL are applicable instead of those
+# above. If you wish to allow use of your version of this file only
+# under the terms of the GPL, and not to allow others to use your
+# version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the
+# notice and other provisions required by the GPL. If you do not
+# delete the provisions above, a recipient may use your version of
+# this file under the terms of any one of the MPL or the GPL.
+#
+# ***** END LICENSE BLOCK *****
+
+'''
+Example of simple consumer. Acks each message as it arrives.
+'''
+
+import logging
+import sys
+import pika
+import pika.adapters
+
+logging.basicConfig(level=logging.DEBUG)
+
+connection = None
+channel = None
+
+
+def on_connected(connection):
+
+    global channel
+
+    logging.info("demo_send: Connected to RabbitMQ")
+    channel = connection.channel(on_channel_open)
+
+
+def on_channel_open(channel):
+
+    logging.info("demo_send: Received our Channel")
+    channel.queue_declare(queue="test", durable=True,
+                          exclusive=False, auto_delete=False,
+                          callback=on_queue_declared)
+
+
+def on_queue_declared():
+
+    logging.info("demo_send: Queue Declared")
+    channel.basic_consume(handle_delivery, queue='test', consumer_tag='ctag0')
+
+
+def on_basic_cancel():
+
+    logging.info("Basic.Consume cancelled")
+    connection.close()
+
+
+def handle_delivery(channel, method, header, body):
+
+    logging.info("demo_send.handle_delivery")
+    logging.info("method=%r" % method)
+    logging.info("header=%r" % header)
+    logging.info("  body=%r" % body)
+    channel.basic_ack(delivery_tag=method.delivery_tag)
+
+
+if __name__ == '__main__':
+
+    host = (len(sys.argv) > 1) and sys.argv[1] or '127.0.0.1'
+    parameters = pika.ConnectionParameters(host)
+
+    strategy = pika.reconnection_strategies.SimpleReconnectionStrategy()
+
+    ioloop = pika.adapters.IOLoop.instance()
+
+    connection = pika.adapters.SelectConnection(parameters, on_connected,
+                                                reconnection_strategy=strategy)
+
+    try:
+        ioloop.start()
+    except KeyboardInterrupt:
+        channel.basic_cancel('ctag0', on_basic_cancel)
+        ioloop.start()
