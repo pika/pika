3efdfc07f5c9a2468b131e69a205213e6fcbbc68
Gavin M. Roy <gmr@myyearbook.com>
2011-01-25 17:16:29 -0500

    Replace fast_receive with timed_receive

---
+++ examples/timed_receive.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1/GPL 2.0
+#
+# The contents of this file are subject to the Mozilla Public License
+# Version 1.1 (the "License"); you may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS"
+# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+# the License for the specific language governing rights and
+# limitations under the License.
+#
+# The Original Code is Pika.
+#
+# The Initial Developers of the Original Code are LShift Ltd, Cohesive
+# Financial Technologies LLC, and Rabbit Technologies Ltd.  Portions
+# created before 22-Nov-2008 00:00:00 GMT by LShift Ltd, Cohesive
+# Financial Technologies LLC, or Rabbit Technologies Ltd are Copyright
+# (C) 2007-2008 LShift Ltd, Cohesive Financial Technologies LLC, and
+# Rabbit Technologies Ltd.
+#
+# Portions created by LShift Ltd are Copyright (C) 2007-2009 LShift
+# Ltd. Portions created by Cohesive Financial Technologies LLC are
+# Copyright (C) 2007-2009 Cohesive Financial Technologies
+# LLC. Portions created by Rabbit Technologies Ltd are Copyright (C)
+# 2007-2009 Rabbit Technologies Ltd.
+#
+# Portions created by Tony Garnock-Jones are Copyright (C) 2009-2010
+# LShift Ltd and Tony Garnock-Jones.
+#
+# All Rights Reserved.
+#
+# Contributor(s): ______________________________________.
+#
+# Alternatively, the contents of this file may be used under the terms
+# of the GNU General Public License Version 2 or later (the "GPL"), in
+# which case the provisions of the GPL are applicable instead of those
+# above. If you wish to allow use of your version of this file only
+# under the terms of the GPL, and not to allow others to use your
+# version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the
+# notice and other provisions required by the GPL. If you do not
+# delete the provisions above, a recipient may use your version of
+# this file under the terms of any one of the MPL or the GPL.
+#
+# ***** END LICENSE BLOCK *****
+
+'''
+Example of simple consumer. Acks each message as it arrives.
+'''
+
+import logging
+import pika
+import sys
+import time
+
+# Import all adapters for easier experimentation
+from pika.adapters import *
+
+logging.basicConfig(level=logging.INFO)
+
+channel = None
+connection = None
+count = 0
+last_count = 0
+start_time = None
+
+def on_connected(connection):
+    global channel
+    logging.info("demo_send: Connected to RabbitMQ")
+    connection.channel(on_channel_open)
+
+
+def on_channel_open(channel_):
+    global channel
+    channel = channel_
+    logging.info("demo_send: Received our Channel")
+    channel.queue_declare(queue="test", durable=True,
+                          exclusive=False, auto_delete=False,
+                          callback=on_queue_declared)
+
+
+def on_queue_declared(frame):
+    global start_time
+    logging.info("demo_send: Queue Declared")
+    start_time = time.time()
+    channel.basic_consume(handle_delivery, queue='test', no_ack=True)
+
+
+def handle_delivery(channel, method, header, body):
+    global count, last_count, start_time
+    count += 1
+    if count % 1000 == 0:
+        now = time.time()
+        duration = now - start_time
+        sent = count - last_count
+        rate = sent / duration
+        last_count = count
+        start_time = now
+        logging.info("timed_receive: %i Messages Received, %.4f per second" %\
+                     (count, rate))
+
+
+
+if __name__ == '__main__':
+
+    host = (len(sys.argv) > 1) and sys.argv[1] or '127.0.0.1'
+    parameters = pika.ConnectionParameters(host)
+
+    strategy = pika.reconnection_strategies.SimpleReconnectionStrategy()
+
+    connection = SelectConnection(parameters, on_connected,
+                                  reconnection_strategy=strategy)
+    try:
+        connection.ioloop.start()
+    except KeyboardInterrupt:
+        connection.close()
+        connection.ioloop.start()
