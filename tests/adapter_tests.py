c5ee840f5640d6ea11c6a6f17f1c4482b7c16f63
Gavin M. Roy <gmr@myyearbook.com>
2011-01-25 21:59:00 -0500

    Got screwy with the name of the test there for a while. Put all asynchronous adapters in the test, BlockingConnection needs its own

--- tests/adapter_tests.py
+++ tests/adapter_tests.py
@@ -7,7 +7,8 @@ First test to make sure all adapters can connect and disconnect properly
 
 import nose
 import pika
-import pika.adapters
+import pika.adapters as adapters
+
 from pika.adapters.tornado_connection import TornadoConnection
 
 connected = False  # Global connected flag, resets in each test
@@ -15,105 +16,93 @@ connected = False  # Global connected flag, resets in each test
 HOST = 'localhost'
 PORT = 5672
 
-"""
-Hanging
 @nose.tools.timed(2)
 def test_asyncore_connection():
-    global connected
-    connected = False
-    pika.adapters.select_connection.SELECT_TYPE = 'epoll'
-    parameters = pika.ConnectionParameters(HOST, PORT)
-    connection = pika.adapters.AsyncoreConnection(parameters,
-                                                  on_connected)
+    connection = connect(adapters.AsyncoreConnection)
     connection.ioloop.start()
     if not connected:
         assert False
     else:
         pass
-"""
 
 @nose.tools.timed(2)
 def test_select_epoll_connection():
-    global connected
-    connected = False
-    pika.adapters.select_connection.SELECT_TYPE = 'epoll'
-    parameters = pika.ConnectionParameters(HOST, PORT)
-    connection = pika.adapters.SelectConnection(parameters,
-                                                on_connected)
+    set_select_poller('epoll')
+    connection = connect(adapters.SelectConnection)
     connection.ioloop.start()
-    if not isinstance(connection.ioloop.poller,
-                      pika.adapters.select_connection.EPollPoller):
+    if connection.ioloop.get_poller_type() != 'EpollPoller':
         assert False
-    if not connected:
+    if not is_connected():
         assert False
-    else:
-        pass
+    pass
+
 
 @nose.tools.timed(2)
 def test_select_poll_connection():
-    global connected
-    connected = False
-    pika.adapters.select_connection.SELECT_TYPE = 'poll'
-    parameters = pika.ConnectionParameters(HOST, PORT)
-    connection = pika.adapters.SelectConnection(parameters,
-                                                on_connected)
+    set_select_poller('poll')
+    connection = connect(adapters.SelectConnection)
     connection.ioloop.start()
-    if not isinstance(connection.ioloop.poller,
-                      pika.adapters.select_connection.PollPoller):
+    if connection.ioloop.get_poller_type() != 'PollPoller':
         assert False
-    if not connected:
+    if not is_connected():
         assert False
-    else:
-        pass
+    pass
+
 
 @nose.tools.timed(2)
 def test_select_select_connection():
-    global connected
-    connected = False
-    pika.adapters.select_connection.SELECT_TYPE = 'select'
-    parameters = pika.ConnectionParameters(HOST, PORT)
-    connection = pika.adapters.SelectConnection(parameters,
-                                                on_connected)
+    set_select_poller('select')
+    connection = connect(adapters.SelectConnection)
     connection.ioloop.start()
-    if not isinstance(connection.ioloop.poller,
-                      pika.adapters.select_connection.SelectPoller):
+    if connection.ioloop.get_poller_type() != 'SelectPoller':
         assert False
-    if not connected:
+    if not is_connected():
         assert False
-    else:
-        pass
+    pass
 
 @nose.tools.timed(2)
 def test_select_kqueue_connection():
     global connected
-    connected = False
-    pika.adapters.select_connection.SELECT_TYPE = 'kqueue'
-    parameters = pika.ConnectionParameters(HOST, PORT)
-    connection = pika.adapters.SelectConnection(parameters,
-                                                on_connected)
+    set_select_poller('kqueue')
+    connection = connect(adapters.SelectConnection)
     connection.ioloop.start()
-    if not isinstance(connection.ioloop.poller,
-                      pika.adapters.select_connection.KQueuePoller):
+    if connection.ioloop.get_poller_type() != 'KQueuePoller':
         assert False
-    if not connected:
+    if not is_connected():
+        assert False
+    pass
+
+@nose.tools.timed(2)
+def test_select_tornado_connection():
+    connection = connect(TornadoConnection)
+    connection.ioloop.start()
+    if not is_connected():
         assert False
     else:
         pass
 
-@nose.tools.timed(2)
-def test_select_tornado_connection():
+@nose.tools.nottest
+def connect(connection_type):
     global connected
     connected = False
     parameters = pika.ConnectionParameters(HOST, PORT)
-    connection = TornadoConnection(parameters, on_connected)
-    connection.ioloop.start()
-    if not connected:
-        assert False
-    else:
-        pass
+    return connection_type(parameters, on_connected)
+
+@nose.tools.nottest
+def set_select_poller(type):
+    adapters.select_connection.SELECT_TYPE = type
+
+@nose.tools.nottest
+def is_connected():
+    global connected
+    return connected
 
 @nose.tools.nottest
 def on_connected(connection):
     global connected
     connected = connection.is_open()
     connection.ioloop.stop()
+
+if __name__ == '__main__':
+
+    test_select_kqueue_connection()
