dba708358cc3b3ff52df1aa5cb8a69837d8bab1a
Gavin M. Roy <gmr@myyearbook.com>
2011-01-28 16:23:00 -0500

    Reflect the move from codec.Frame* to frames.* and the move from codec.ConnectionState into this module

--- pika/connection.py
+++ pika/connection.py
@@ -47,18 +47,18 @@
 # ***** END LICENSE BLOCK *****
 
 import logging
+import struct
 
 import pika.channel as channel
-import pika.codec as codec
+import pika.frames as frames
 import pika.simplebuffer as simplebuffer
 import pika.spec as spec
 
-from pika.specbase import _codec_repr
-from pika.exceptions import *
-
 from pika.callback import CallbackManager
 from pika.credentials import PlainCredentials
+from pika.exceptions import *
 from pika.heartbeat import HeartbeatChecker
+from pika.object import object_
 from pika.reconnection_strategies import NullReconnectionStrategy
 
 CHANNEL_MAX = 32767
@@ -70,7 +70,7 @@ PRODUCT = "Pika Python AMQP Client Library"
 default_credentials = PlainCredentials('guest', 'guest')
 
 
-class ConnectionParameters(object):
+class ConnectionParameters(object_):
 
     def __init__(self,
                  host,
@@ -89,12 +89,8 @@ class ConnectionParameters(object):
         self.frame_max = frame_max
         self.heartbeat = heartbeat
 
-    def __repr__(self):
 
-        return _codec_repr(self, lambda: ConnectionParameters(None))
-
-
-class Connection(object):
+class Connection(object_):
 
     """
     Pika Connection Class
@@ -204,7 +200,7 @@ class Connection(object):
 
         # Connection state, server properties and channels all change on
         # each connection
-        self.state = codec.ConnectionState()
+        self.state = ConnectionState()
         self.server_properties = None
         self._channels = dict()
 
@@ -226,9 +222,9 @@ class Connection(object):
         Returns the Frame Protocol Header for our AMQP Client for communicating
         with our AMQP Broker
         """
-        return codec.FrameProtocolHeader(1, 1,
-                                         spec.PROTOCOL_VERSION[0],
-                                         spec.PROTOCOL_VERSION[1])
+        return frames.ProtocolHeader(1, 1,
+                                     spec.PROTOCOL_VERSION[0],
+                                     spec.PROTOCOL_VERSION[1])
 
     def connect(self, host, port):
         """
@@ -336,7 +332,7 @@ class Connection(object):
         self.closed = False
 
         # We didn't expect a FrameProtocolHeader, did we get one?
-        if isinstance(frame, codec.FrameProtocolHeader):
+        if isinstance(frame,frames.ProtocolHeader):
             raise ProtocolVersionMismatch(self._local_protocol_header(), frame)
 
         # Make sure that the major and minor version matches our spec version
@@ -575,7 +571,11 @@ class Connection(object):
 
         while data:
 
-            (consumed_count, frame) = self.state.handle_input(data)
+            try:
+                (consumed_count, frame) = self.state.handle_input(data)
+            except  InvalidFrameError:
+                # We didn't get a full frame so break
+                break
 
             # If we don't have a full frame, set our global buffer and exit
             if not frame:
@@ -589,7 +589,7 @@ class Connection(object):
             self.bytes_received += consumed_count
 
             # If we have a Method Frame and have callbacks for it
-            if isinstance(frame, codec.FrameMethod) and \
+            if isinstance(frame, frames.Method) and \
                 self.callbacks.pending(frame.channel_number, frame.method):
 
                 # Process the callbacks for it
@@ -601,7 +601,7 @@ class Connection(object):
             # We don't check for heartbeat frames because we can not count
             # atomic frames reliably due to different message behaviors
             # such as large content frames being transferred slowly
-            elif isinstance(frame, codec.FrameHeartbeat):
+            elif isinstance(frame, frames.Heartbeat):
                 continue
 
             elif frame.channel_number > 0:
@@ -652,7 +652,7 @@ class Connection(object):
                                                       channel_number,
                                                       method,
                                                       content))
-        self._send_frame(codec.FrameMethod(channel_number, method))
+        self._send_frame(frames.Method(channel_number, method))
 
         if isinstance(content, tuple):
             props = content[0]
@@ -665,18 +665,18 @@ class Connection(object):
             length = 0
             if body:
                 length = len(body)
-            self._send_frame(codec.FrameHeader(channel_number, length, props))
+            self._send_frame(frames.Header(channel_number, length, props))
 
         if body:
             max_piece = (self.state.frame_max - \
-                         codec.ConnectionState.HEADER_SIZE - \
-                         codec.ConnectionState.FOOTER_SIZE)
+                         ConnectionState.HEADER_SIZE - \
+                         ConnectionState.FOOTER_SIZE)
             body_buf = simplebuffer.SimpleBuffer(body)
 
             while body_buf:
                 piece_len = min(len(body_buf), max_piece)
                 piece = body_buf.read_and_consume(piece_len)
-                self._send_frame(codec.FrameBody(channel_number, piece))
+                self._send_frame(frames.Body(channel_number, piece))
 
     def suggested_buffer_size(self):
         """
@@ -687,3 +687,111 @@ class Connection(object):
             return FRAME_MAX
 
         return self.state.frame_max
+
+
+
+class ConnectionState:
+
+    HEADER_SIZE = 7
+    FOOTER_SIZE = 1
+
+    def __init__(self):
+
+        self.channel_max = None
+        self.frame_max = None
+        self._return_to_idle()
+
+    def tune(self, channel_max, frame_max):
+
+        self.channel_max = channel_max
+        self.frame_max = frame_max
+
+    def _return_to_idle(self):
+
+        self.inbound_buffer = list()
+        self.inbound_available = 0
+        self.target_size = ConnectionState.HEADER_SIZE
+        self.state = self._waiting_for_header
+
+    def _inbound(self):
+
+        return ''.join(self.inbound_buffer)
+
+    def handle_input(self, received_data):
+
+        total_bytes_consumed = 0
+
+        while True:
+            if not received_data:
+                return total_bytes_consumed, None
+
+            bytes_consumed = self.target_size - self.inbound_available
+            if len(received_data) < bytes_consumed:
+                bytes_consumed = len(received_data)
+
+            self.inbound_buffer.append(received_data[:bytes_consumed])
+            self.inbound_available += bytes_consumed
+            received_data = received_data[bytes_consumed:]
+            total_bytes_consumed += bytes_consumed
+
+            if self.inbound_available < self.target_size:
+                return total_bytes_consumed, None
+
+            maybe_result = self.state(self._inbound())
+            if maybe_result:
+                return total_bytes_consumed, maybe_result
+
+    def _waiting_for_header(self, inbound):
+        # Here we switch state without resetting the inbound_buffer,
+        # because we want to keep the frame header.
+
+        if inbound[:3] == 'AMQ':
+            # Protocol header.
+            self.target_size = 8
+            self.state = self._waiting_for_protocol_header
+        else:
+            self.target_size = struct.unpack_from('>I', inbound, 3)[0] + \
+                               ConnectionState.HEADER_SIZE + \
+                               ConnectionState.FOOTER_SIZE
+            self.state = self._waiting_for_body
+
+    def _waiting_for_body(self, inbound):
+
+        if ord(inbound[-1]) != spec.FRAME_END:
+            raise InvalidFrameError("Invalid frame end byte", inbound[-1])
+
+        self._return_to_idle()
+
+        (frame_type, channel_number) = struct.unpack_from('>BH', inbound, 0)
+        if frame_type == spec.FRAME_METHOD:
+            method_id = struct.unpack_from('>I', inbound,
+                                           ConnectionState.HEADER_SIZE)[0]
+            method = spec.methods[method_id]()
+            method.decode(inbound, ConnectionState.HEADER_SIZE + 4)
+            return frames.Method(channel_number, method)
+        elif frame_type == spec.FRAME_HEADER:
+            (class_id, body_size) = \
+                struct.unpack_from('>HxxQ', inbound,
+                                   ConnectionState.HEADER_SIZE)
+            props = spec.props[class_id]()
+            props.decode(inbound, ConnectionState.HEADER_SIZE + 12)
+            return frames.Header(channel_number, body_size, props)
+        elif frame_type == spec.FRAME_BODY:
+            return frames.Body(channel_number,
+                                   inbound[ConnectionState.HEADER_SIZE: \
+                                           -ConnectionState.FOOTER_SIZE])
+        elif frame_type == spec.FRAME_HEARTBEAT:
+            return frames.Heartbeat()
+
+        # Ignore the frame
+        return None
+
+    def _waiting_for_protocol_header(self, inbound):
+
+        if inbound[3] != 'P':
+            raise InvalidProtocolHeader(inbound)
+
+        self._return_to_idle()
+
+        (th, tl, vh, vl) = struct.unpack_from('BBBB', inbound, 4)
+        return frames.ProtocolHeader(th, tl, vh, vl)
