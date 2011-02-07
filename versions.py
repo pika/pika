1992dd2ff33b25ed6b8027fd49a7b3f9f719f1e2
Gavin M. Roy <gmr@myyearbook.com>
2011-02-06 19:04:56 -0500

    Fix the versions.py command

--- versions.py
+++ versions.py
@@ -7,14 +7,15 @@ warnings.filterwarnings('ignore', category=DeprecationWarning)
 def test_for_version(filename):
     stdin, stdout = os.popen4('%s -V' % filename, 'r')
     response = stdout.read()
+    if response.find('command not found') > 0:
+        return False
     return '.'.join(response.strip().split(' ')[1].split('.')[:-1])
 
-
-versions = ['python', 'python2.4', 'python2.5', 'python2.6']
+versions = ['python', 'python2.3', 'python2.4', 'python2.5', 'python2.6']
 valid = {}
 for filename in versions:
     version = test_for_version(filename)
-    if version not in valid:
+    if version and version not in valid:
         valid[version] = filename
 
 # Prefer the latest version of python
