1f253c5bdf90055ff2d00a3b8d18c152c3b7031f
Gavin M. Roy <gmr@myyearbook.com>
2011-02-12 11:16:39 -0500

    Check for Python 2.7 as well

--- versions.py
+++ versions.py
@@ -11,14 +11,14 @@ def test_for_version(filename):
         return False
     return '.'.join(response.strip().split(' ')[1].split('.')[:-1])
 
-versions = ['python', 'python2.4', 'python2.5', 'python2.6']
+versions = ['python', 'python2.4', 'python2.5', 'python2.6', 'python2.7']
 valid = {}
 for filename in versions:
     version = test_for_version(filename)
     if version and version not in valid:
         valid[version] = filename
 
-# Prefer the latest version of python
+# Prefer 2.6 major version of python since that's my main development env
 output = []
 if '2.6' in valid:
     output.append(valid['2.6'])
