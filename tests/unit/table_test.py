b0052d05acc879bd00f89e71c73b2cd788962b15
Brian K. Jones <bkjones@gmail.com>
2011-02-25 03:10:01 +0800

    Removed nose dependency from table_test.py

--- tests/unit/table_test.py
+++ tests/unit/table_test.py
@@ -1,50 +1,8 @@
 # ***** BEGIN LICENSE BLOCK *****
-# Version: MPL 1.1/GPL 2.0
 #
-# The contents of this file are subject to the Mozilla Public License
-# Version 1.1 (the "License"); you may not use this file except in
-# compliance with the License. You may obtain a copy of the License at
-# http://www.mozilla.org/MPL/
+# For copyright and licensing please refer to COPYING.
 #
-# Software distributed under the License is distributed on an "AS IS"
-# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
-# the License for the specific language governing rights and
-# limitations under the License.
-#
-# The Original Code is Pika.
-#
-# The Initial Developers of the Original Code are LShift Ltd, Cohesive
-# Financial Technologies LLC, and Rabbit Technologies Ltd.  Portions
-# created before 22-Nov-2008 00:00:00 GMT by LShift Ltd, Cohesive
-# Financial Technologies LLC, or Rabbit Technologies Ltd are Copyright
-# (C) 2007-2008 LShift Ltd, Cohesive Financial Technologies LLC, and
-# Rabbit Technologies Ltd.
-#
-# Portions created by LShift Ltd are Copyright (C) 2007-2009 LShift
-# Ltd. Portions created by Cohesive Financial Technologies LLC are
-# Copyright (C) 2007-2009 Cohesive Financial Technologies
-# LLC. Portions created by Rabbit Technologies Ltd are Copyright (C)
-# 2007-2009 Rabbit Technologies Ltd.
-#
-# Portions created by Tony Garnock-Jones are Copyright (C) 2009-2010
-# LShift Ltd and Tony Garnock-Jones.
-#
-# All Rights Reserved.
-#
-# Contributor(s): ______________________________________.
-#
-# Alternatively, the contents of this file may be used under the terms
-# of the GNU General Public License Version 2 or later (the "GPL"), in
-# which case the provisions of the GPL are applicable instead of those
-# above. If you wish to allow use of your version of this file only
-# under the terms of the GPL, and not to allow others to use your
-# version of this file under the terms of the MPL, indicate your
-# decision by deleting the provisions above and replace them with the
-# notice and other provisions required by the GPL. If you do not
-# delete the provisions above, a recipient may use your version of
-# this file under the terms of any one of the MPL or the GPL.
-#
-# ***** END LICENSE BLOCK *****
+# ***** END LICENSE BLOCK *****#
 
 """
 Pika table tests
@@ -56,8 +14,8 @@ sys.path.append('..')
 sys.path.append(os.path.join('..', '..'))
 
 import datetime
-import nose
 import pika.table
+import unittest
 
 from decimal import Decimal
 
@@ -86,69 +44,50 @@ def encode_table():
     '\x00\x00\x00\x08\x01at\x01\x01bt\x00'
     '''
 
+class TestTable(unittest.TestCase):
+    def test_reencode_none(self):
+        self.assertEqual(self.reencode(None), {})
 
-def test_reencode_none():
-    if not reencode(None) == {}:
-        assert False
+    def test_reencode_ints(self):
+        value = {'a': 1}
+        self.assertEqual(self.reencode(value), value)
 
+    def test_reencode_mixed(self):
+        value = {'a': 1, 'c': 1, 'e': {}, 'd': 'x', 'f': -1}
+        self.assertEqual(self.reencode(value), value)
 
-def test_reencode_ints():
-    value = {'a': 1}
-    if not reencode(value) == value:
-        assert False
+    def test_reencode_datetime(self):
+        value = {'a': datetime.datetime(2010, 12, 31, 23, 58, 59)}
+        self.assertEqual(self.reencode(value), value)
 
+    def test_reencode_long(self):
+        value = {'a': 9128161957192253167L, 'b': -9128161957192253167L}
+        self.assertEqual(self.reencode(value), value)
 
-def test_reencode_mixed():
-    value = {'a': 1, 'c': 1, 'e': {}, 'd': 'x', 'f': -1}
-    if not reencode(value) == value:
-        assert False
+    def test_reencode_negative_decimals(self):
+        value = {'a': 1, 'b': Decimal('-1.234'), 'g': -1}
+        self.assertEqual(self.reencode(value), value)
 
+    def test_reencode_mixed_with_decimals(self):
+        value = {'a': [1, 2, 3, 'a', Decimal('-0.01'), 5]}
+        self.assertEqual(self.reencode(value), value)
 
-def test_reencode_datetime():
-    value = {'a': datetime.datetime(2010, 12, 31, 23, 58, 59)}
-    if not reencode(value) == value:
-        assert False
+    def test_reencode_bool(self):
+        value = {'a': True, 'b': False}
+        self.assertEqual(self.reencode(value), value)
 
+    def encode(self, v):
+        p = []
+        n = pika.table.encode_table(p, v)
+        r = ''.join(p)
+        assert len(r) == n
+        return r
 
-def test_reencode_long():
-    value = {'a': 9128161957192253167L, 'b': -9128161957192253167L}
-    if not reencode(value) == value:
-        assert False
-
-
-def test_reencode_negative_decimals():
-    value = {'a': 1, 'b': Decimal('-1.234'), 'g': -1}
-    if not reencode(value) == value:
-        assert False
-
-
-def test_reencode_mixed_with_decimals():
-    value = {'a': [1, 2, 3, 'a', Decimal('-0.01'), 5]}
-    if not reencode(value) == value:
-        assert False
-
-
-def test_reencode_bool():
-    value = {'a': True, 'b': False}
-    if not reencode(value) == value:
-        assert False
-
-
-@nose.tools.nottest
-def encode(v):
-    p = []
-    n = pika.table.encode_table(p, v)
-    r = ''.join(p)
-    assert len(r) == n
-    return r
-
-
-@nose.tools.nottest
-def reencode(i):
-    r = encode(i)
-    (v, n) = pika.table.decode_table(r, 0)
-    assert len(r) == n
-    return v
+    def reencode(self, i):
+        r = self.encode(i)
+        (v, n) = pika.table.decode_table(r, 0)
+        assert len(r) == n
+        return v
 
 if __name__ == "__main__":
-    nose.runmodule(argv=['-s', '--with-doctest', '--doctest-tests'])
+    unittest.main()
