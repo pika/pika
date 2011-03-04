7bbfa4aeb00e0dc7c5a3c34e70b5164b41dd8165
Gavin M. Roy <gmr@myyearbook.com>
2011-03-04 00:18:21 -0500

    Split out unit tests and functional tests, run all unittests for unit under one nosetests call

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -9,6 +9,16 @@ Simple test runner to separate out the functional tests and the unit tests.
 """
 import os
 import subprocess
+import sys
+
+nosetests = True
+functional = True
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == 'nosetests':
+        functional = False
+    elif sys.argv[1] == "functional":
+        nosetests = False
 
 PLATFORMS = ['bsd', 'linux', 'nt']
 
@@ -21,7 +31,8 @@ except AttributeError:
 if platform == 'darwin':
     platform = 'bsd'
 
-DIRECTORIES = ['functional', 'unit']  # Unit tests to be added here
+INDIVIDUAL = ['functional']  # Unit tests to be added here
+NOSETESTS = ['unit']
 
 
 def platform_test(filename):
@@ -52,14 +63,24 @@ def run_test(file):
         return True
     return False
 
+if nosetests:
+    for directory in NOSETESTS:
+        print "Running nosetests in %s" % directory
+        print
+        proc = subprocess.Popen("cd %s && nosetests" % directory,
+                                shell=True,
+                                stdout=subprocess.PIPE)
+        output = proc.communicate()[0]
+        print output
 
-for directory in DIRECTORIES:
-    files = os.listdir(directory)
-    for file in files:
-        file_path = os.path.join(directory, file)
-        if os.path.isfile('%s' % file_path) and file[-3:] == '.py':
-            if platform_test(file):
-                if this_platform(file):
+if functional:
+    for directory in INDIVIDUAL:
+        files = os.listdir(directory)
+        for file in files:
+            file_path = os.path.join(directory, file)
+            if os.path.isfile('%s' % file_path) and file[-3:] == '.py':
+                if platform_test(file):
+                    if this_platform(file):
+                        run_test(file_path)
+                else:
                     run_test(file_path)
-            else:
-                run_test(file_path)
