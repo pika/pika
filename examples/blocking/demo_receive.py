70f9cd8a7486e600f422fecb3ec58574b0f96b42
Gavin M. Roy <gmr@myyearbook.com>
2011-03-06 18:08:53 -0500

    Update to reflect the new behavior in BlockingChannel and remote pika.log for output. Simplify pika imports.

--- examples/blocking/demo_receive.py
+++ examples/blocking/demo_receive.py
@@ -8,34 +8,42 @@
 Example of simple consumer. Acks each message as it arrives.
 '''
 # Import all adapters for easier experimentation
-import pika
 import sys
 
 from pika.adapters import BlockingConnection
-
-pika.log.setup(color=True)
-
+from pika.connection import ConnectionParameters
 
 def handle_delivery(channel, method_frame, header_frame, body):
     # Receive the data in 3 frames from RabbitMQ
-    pika.log.info("Basic.Deliver %s delivery-tag %i: %s",
-                  header_frame.content_type,
-                  method_frame.delivery_tag,
-                  body)
+    print "demo_receive: Basic.Deliver %s delivery-tag %i: %s" % \
+          (header_frame.content_type,
+           method_frame.delivery_tag,
+           body)
+
+    # Acknowledge the message
     channel.basic_ack(delivery_tag=method_frame.delivery_tag)
 
 if __name__ == '__main__':
+
     # Connect to RabbitMQ
     host = (len(sys.argv) > 1) and sys.argv[1] or '127.0.0.1'
-    parameters = pika.ConnectionParameters(host)
-    connection = BlockingConnection(parameters)
+    connection = BlockingConnection(ConnectionParameters(host))
 
     # Open the channel
     channel = connection.channel()
 
     # Declare the queue
-    channel.queue_declare(queue="test", durable=True,
-                          exclusive=False, auto_delete=False)
+    channel.queue_declare(queue="test",
+                          durable=True,
+                          exclusive=False,
+                          auto_delete=False)
 
-    # We're stuck looping here since this is a blocking adapter
-    channel.basic_consume(handle_delivery, queue='test')
+    # Add a queue to consume
+    consumer_tag = channel.basic_consume(handle_delivery, queue='test')
+
+    # Start consuming, block until keyboard interrupt
+    try:
+        channel.start_consuming()
+    except KeyboardInterrupt:
+        channel.stop_consuming()
+        connection.close()
