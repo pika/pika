5d03729e9bfadea11d415960526e984beb58173f
Gavin M. Roy <gmr@meetme.com>
2012-09-29 22:38:53 -0400

    Cleanup and slight refactor of CallbackManager
    - Removed the ability for ONLY_CALLER to match on a Class, should only match on an instance
    - Add full test coverage

--- pika/callback.py
+++ pika/callback.py
@@ -8,33 +8,96 @@ Callback management class, common area for keeping track of all callbacks in
 the Pika stack.
 
 """
+import functools
 import logging
+
+from pika import frame
+from pika import amqp_object
 
 LOGGER = logging.getLogger(__name__)
 
 
-def _name_or_value(key):
+def _name_or_value(value):
     """Will take Frame objects, classes, etc and attempt to return a valid
     string identifier for them.
 
-    :param object|str|dict key: The key to sanitize
+    :param value: The value to sanitize
+    :type value:  pika.amqp_object.AMQPObject|pika.frame.Frame|int|unicode|str
     :rtype: str
 
     """
-    # Is it a Pika AMQP Method class with a NAME attribute?
-    if hasattr(key, 'method') and hasattr(key.method, 'NAME'):
-        return key.method.NAME
+    # Is it subclass of AMQPObject
+    try:
+        if issubclass(value, amqp_object.AMQPObject):
+            return value.NAME
+    except TypeError:
+        pass
 
-    # Is it a Pika object with a name attribute?
-    if hasattr(key, 'NAME'):
-        return key.NAME
+    # Is it a Pika frame object?
+    if isinstance(value, frame.Method):
+        return value.method.NAME
 
-    # Is it a dictionary with a name key
-    if hasattr(key, '__dict__') and 'NAME' in key.__dict__:
-        return key.__dict__['NAME']
+    # Is it a Pika frame object (go after Method since Method extends this)
+    if isinstance(value, amqp_object.AMQPObject):
+        return value.NAME
 
-    # Return the item as a string
-    return str(key)
+    # Cast the value to a string, encoding it if it's unicode
+    try:
+        return str(value)
+    except UnicodeEncodeError:
+        return str(value.encode('utf-8'))
+
+
+def sanitize_prefix(function):
+    """Automatically call _name_or_value on the prefix passed in."""
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+        LOGGER.debug('Args: %r, kwargs: %r', args, kwargs)
+        args = list(args)
+        offset = 1
+        if 'prefix' in kwargs:
+            kwargs['prefix'] = _name_or_value(kwargs['prefix'])
+        elif len(args) - 1  >= offset:
+            args[offset] = _name_or_value(args[offset])
+            offset += 1
+        if 'key' in kwargs:
+            kwargs['key'] = _name_or_value(kwargs['key'])
+        elif len(args) - 1 >= offset:
+            args[offset] = _name_or_value(args[offset])
+
+        return function(*tuple(args), **kwargs)
+    return wrapper
+
+
+def check_for_prefix_and_key(function):
+    """Automatically return false if the key or prefix is not in the callbacks
+    for the instance.
+
+    """
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+        offset = 1
+        # Sanitize the prefix
+        if 'prefix' in kwargs:
+            prefix = _name_or_value(kwargs['prefix'])
+        else:
+            prefix = _name_or_value(args[offset])
+            offset += 1
+
+        # Make sure to sanitize the key as well
+        if 'key' in kwargs:
+            key = _name_or_value(kwargs['key'])
+        else:
+            key = _name_or_value(args[offset])
+
+        # Make sure prefix and key are in the stack
+        if (prefix not in args[0]._stack or
+            key not in args[0]._stack[prefix]):
+            return False
+
+        # Execute the method
+        return function(*args, **kwargs)
+    return wrapper
 
 
 class CallbackManager(object):
@@ -44,11 +107,16 @@ class CallbackManager(object):
     by the CallbackManager.instance() method instead of constructing new
     instances of it.
     """
+    DUPLICATE_WARNING = 'Duplicate callback found for "%s:%s"'
+    CALLBACK = 'callback'
+    ONE_SHOT = 'one_shot'
+    ONLY_CALLER = 'only'
 
     def __init__(self):
         """Create an instance of the CallbackManager"""
-        self._callbacks = dict()
+        self._stack = dict()
 
+    @sanitize_prefix
     def add(self, prefix, key, callback, one_shot=True, only_caller=None):
         """Add a callback to the stack for the specified key. If the call is
         specified as one_shot, it will be removed after being fired
@@ -67,62 +135,67 @@ class CallbackManager(object):
         :rtype: tuple(prefix, key)
 
         """
-        # Lets not use objects, since we could have object/class issues
-        key = _name_or_value(key)
+        # Prep the stack
+        if prefix not in self._stack:
+            self._stack[prefix] = dict()
+        if key not in self._stack[prefix]:
+            self._stack[prefix][key] = list()
 
-        # Make sure we've seen the prefix before
-        if prefix not in self._callbacks:
-            self._callbacks[prefix] = dict()
+        # Create the callback dictionary
+        callback_dict = self._callback_dict(callback, one_shot, only_caller)
 
-        # If we don't have the key in our callbacks, add it
-        if key not in self._callbacks[prefix]:
-            self._callbacks[prefix][key] = list()
-
-        # Our callback info we need elsewhere in the class
-        callback_dict = {'handle': callback, 'one_shot': one_shot}
-        if only_caller:
-            callback_dict['only'] = only_caller
-
-        # If we passed in that we do not want duplicates, check and keep us
-        # from adding it a second time
-        if callback_dict in self._callbacks[prefix][key]:
-            LOGGER.warning('Duplicate callback found for "%s:%s"', prefix, key)
+        # Check for a duplicate
+        if callback_dict in self._stack[prefix][key]:
+            LOGGER.warning(self.DUPLICATE_WARNING, prefix, key)
             return prefix, key
 
-        # Append the callback to our key list
-        self._callbacks[prefix][key].append(callback_dict)
-        LOGGER.debug('Added "%s:%s" with callback: %s', prefix, key, callback)
+        # Append the callback to the stack
+        self._stack[prefix][key].append(callback_dict)
+        LOGGER.debug('Added "%s:%s" with callback: %r', prefix, key, callback)
         return prefix, key
 
     def clear(self):
         """Clear all the callbacks if there are any defined."""
-        if self._callbacks:
-            LOGGER.debug('Clearing callbacks')
-            self._callbacks = dict()
+        if self._stack:
+            self._stack = dict()
+            LOGGER.debug('Callbacks cleared')
 
+    @sanitize_prefix
+    def cleanup(self, prefix):
+        """Remove all callbacks from the stack by a prefix. Returns True
+        if keys were there to be removed
+
+        :param str prefix: The prefix for keeping track of callbacks with
+        :rtype: bool
+
+        """
+        if prefix not in self._stack or  not self._stack[prefix]:
+            return False
+        del self._stack[prefix]
+
+    @sanitize_prefix
     def pending(self, prefix, key):
         """Return count of callbacks for a given prefix or key or None
 
-        :param [str,int] prefix: Categorize the callback
-        :param [Object,str,dict] key: The key for the callback
+        :param str|int prefix: Categorize the callback
+        :param Object|str|dict key: The key for the callback
         :rtype: None or int
 
         """
-        # Lets not use objects, since we could have module class/obj
-        key = _name_or_value(key)
-
-        if not prefix in self._callbacks or not key in self._callbacks[prefix]:
+        if not prefix in self._stack or not key in self._stack[prefix]:
             return None
-        return len(self._callbacks[prefix][key])
+        return len(self._stack[prefix][key])
 
+    @sanitize_prefix
+    @check_for_prefix_and_key
     def process(self, prefix, key, caller, *args, **keywords):
         """Run through and process all the callbacks for the specified keys.
         Caller should be specified at all times so that callbacks which
         require a specific function to call CallbackManager.process will
         not be processed.
 
-        :param [str,int] prefix: Categorize the callback
-        :param [Object,str,dict] key: The key for the callback
+        :param str|int prefix: Categorize the callback
+        :param Object|str|dict key: The key for the callback
         :param Object caller: Who is firing the event
         :param list args: Any optional arguments
         :param dict keywords: Optional keyword arguments
@@ -130,105 +203,79 @@ class CallbackManager(object):
 
 
         """
-        # Lets not use objects, since we could have module class/obj
-        key = _name_or_value(key)
+        LOGGER.debug('Processing %s:%s', prefix, key)
 
-        # Make sure we have a callback for this event
-        if prefix not in self._callbacks or key not in self._callbacks[prefix]:
-            return False
+        callbacks, removals = list(), list()
+        # Check each callback, append it to the list if it should be called
+        for callback_dict in self._stack[prefix][key]:
+            if self._should_process_callback(callback_dict, caller):
+                callbacks.append(callback_dict[self.CALLBACK])
+                # Remove from the stack if it's a one shot callback
+                if callback_dict[self.ONE_SHOT]:
+                    removals.append([prefix, key, callback_dict[self.CALLBACK]])
 
-        callbacks = list()
-        one_shot_remove = list()
-
-        # Loop through callbacks that want all prefixes and what we asked for
-        for callback in self._callbacks[prefix][key]:
-            if 'only' not in callback or callback['only'] == caller.__class__:
-                callbacks.append(callback['handle'])
-
-                # If it's a one shot callback, add it to a list for removal
-                if callback['one_shot']:
-                    one_shot_remove.append([prefix, key, callback])
-
-        # Remove the one shot callbacks that were called
-        for prefix, key, callback in one_shot_remove:
+        # Remove the one shot callbacks
+        for prefix, key, callback in removals:
             self.remove(prefix, key, callback)
 
-        # Prevent recursion
+        # Call each callback
         for callback in callbacks:
             LOGGER.debug('Calling %s for "%s:%s"', callback, prefix, key)
             callback(*args, **keywords)
-
-        # Indicate success
         return True
 
-    def remove(self, prefix, key, callback=None):
+
+    @sanitize_prefix
+    @check_for_prefix_and_key
+    def remove(self, prefix, key, callback_value=None):
         """Remove a callback from the stack by prefix, key and optionally
         the callback itself. If you only pass in prefix and key, all
         callbacks for that prefix and key will be removed.
 
         :param str prefix: The prefix for keeping track of callbacks with
         :param str key: The callback key
-        :param method callback: The method defined to call on callback
+        :param method callback_value: The method defined to call on callback
         :rtype: bool
 
         """
-        # Lets not use objects, since we could have module class/obj
-        key = _name_or_value(key)
-
-        if prefix in self._callbacks and key in self._callbacks[prefix]:
-
-            if callback:
-                # Remove the callback from the _callbacks dict
-                # callback is just a handler
-                if callable(callback):
-                    callbacks = self._callbacks[prefix][key]
-                    for i in xrange(len(callbacks) - 1, -1, -1):
-                        if callbacks[i]['handle'] == callback:
-                            del(callbacks[i])
-                            LOGGER.debug('Removed %s for "%s:%s"',
-                                         callback, prefix, key)
-                # callback is a full dict
-                elif callback in self._callbacks[prefix][key]:
-                    self._callbacks[prefix][key].remove(callback)
-                    LOGGER.debug('Removed %s for "%s:%s"',
-                                 callback, prefix, key)
-
-                # Remove the list from the dict if it's empty
-                if not self._callbacks[prefix][key]:
-                    del(self._callbacks[prefix][key])
-                    LOGGER.debug('Removed empty key "%s:%s"', prefix, key)
-
-                # Remove the prefix if it's empty
-                if not self._callbacks[prefix]:
-                    del(self._callbacks[prefix])
-                    LOGGER.debug('Removed empty prefix "%s"', prefix)
-                return True
-            else:
-                # Remove the list from the dict if it's empty
-                del(self._callbacks[prefix][key])
-                LOGGER.debug('Removed key "%s:%s"', prefix, key)
-
-                # Remove the prefix if it's empty
-                if not self._callbacks[prefix]:
-                    del(self._callbacks[prefix])
-                    LOGGER.debug('Removed empty prefix "%s"', prefix)
-
+        if callback_value:
+            offsets_to_remove = list()
+            for offset in xrange(len(self._stack[prefix][key]), 0, -1):
+                if (callback_value ==
+                        self._stack[prefix][key][offset - 1][self.CALLBACK]):
+                    offsets_to_remove.append(offset - 1)
+            for offset in offsets_to_remove:
+                del self._stack[prefix][key][offset]
+            if not self._stack[prefix][key]:
+                del self._stack[prefix][key]
         else:
-            # If we just passed in a prefix for a key
-            if prefix in self._callbacks and key in self._callbacks[prefix]:
-                del(self._callbacks[prefix][key])
-                LOGGER.debug('Removed all callbacks for "%s:%s"', prefix, key)
-            return True
+            del self._stack[prefix][key]
+        if not self._stack[prefix]:
+            del self._stack[prefix]
+        return True
 
-        # Prefix, Key or Callback could not be found
-        return False
+    def _callback_dict(self, callback, one_shot, only_caller):
+        """Return the callback dictionary.
 
-    def cleanup(self, prefix):
-        """Remove all callbacks from the stack by a prefix.
-
-        :param str prefix: The prefix for keeping track of callbacks with
+        :param method callback: The callback to call
+        :param bool one_shot: Remove this callback after it is called
+        :param object only_caller: Only allow one_caller value to call the
+                                   event that fires the callback.
+        :rtype: dict
 
         """
-        keys = self._callbacks.get(prefix, {}).keys()
-        for key in keys:
-            self.remove(prefix, key)
+        return {self.CALLBACK: callback,
+                self.ONE_SHOT: one_shot,
+                self.ONLY_CALLER: only_caller}
+
+    def _should_process_callback(self, callback_dict, caller):
+        """Returns True if the callback should be processed.
+
+        :param dict callback_dict: The callback configuration
+        :param Object caller: Who is firing the event
+        :rtype: bool
+
+        """
+        return (callback_dict[self.ONLY_CALLER] is None or
+                (callback_dict[self.ONLY_CALLER] and
+                 callback_dict[self.ONLY_CALLER] == caller))
---
+++ tests/callback_tests.py
@@ -0,0 +1,281 @@
+# -*- coding: utf8 -*-
+"""
+File Description
+
+"""
+import logging
+import mock
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+from pika import callback
+from pika import frame
+from pika import spec
+
+
+class CallbackTests(unittest.TestCase):
+
+    KEY = 'Test Key'
+    CALLBACK = callback.CallbackManager.CALLBACK
+    ONE_SHOT = callback.CallbackManager.ONE_SHOT
+    ONLY_CALLER = callback.CallbackManager.ONLY_CALLER
+    PREFIX_CLASS = spec.Basic.Consume
+    PREFIX = 'Basic.Consume'
+
+    def setUp(self):
+        self.obj = callback.CallbackManager()
+        self.callback_mock = mock.Mock()
+        self.mock_caller = mock.Mock()
+
+    def tearDown(self):
+        del self.obj
+        del self.callback_mock
+        del self.mock_caller
+
+    def test_name_or_value_amqpobject_class(self):
+        self.assertEqual(callback._name_or_value(self.PREFIX_CLASS),
+                         self.PREFIX)
+
+    def test_name_or_value_protocol_header(self):
+        self.assertEqual(callback._name_or_value(frame.ProtocolHeader()),
+                         'ProtocolHeader')
+
+    def test_name_or_value_method_frame(self):
+        value = frame.Method(1, self.PREFIX_CLASS())
+        self.assertEqual(callback._name_or_value(value), self.PREFIX)
+
+    def test_name_or_value_str(self):
+        value = 'Test String Value'
+        expectation = value
+        self.assertEqual(callback._name_or_value(value), expectation)
+
+    def test_name_or_value_unicode(self):
+        value = u'Это тест значения'
+        expectation = ('\xd0\xad\xd1\x82\xd0\xbe \xd1\x82\xd0\xb5\xd1\x81\xd1'
+                       '\x82 \xd0\xb7\xd0\xbd\xd0\xb0\xd1\x87\xd0\xb5\xd0\xbd'
+                       '\xd0\xb8\xd1\x8f')
+        self.assertEqual(callback._name_or_value(value), expectation)
+
+    def test_empty_callbacks_on_init(self):
+        self.assertFalse(self.obj._stack)
+
+    def test_sanitize_decorator_with_args_only(self):
+        self.obj.add(self.PREFIX_CLASS, self.KEY, None)
+        self.assertIn(self.PREFIX, self.obj._stack.keys())
+
+    def test_sanitize_decorator_with_kwargs(self):
+        self.obj.add(prefix=self.PREFIX_CLASS, key=self.KEY, callback=None)
+        self.assertIn(self.PREFIX, self.obj._stack.keys())
+
+    def test_sanitize_decorator_with_mixed_args_and_kwargs(self):
+        self.obj.add(self.PREFIX_CLASS, key=self.KEY, callback=None)
+        self.assertIn(self.PREFIX, self.obj._stack.keys())
+
+    def test_callback_add_first_time_prefix_added(self):
+        self.obj.add(self.PREFIX, self.KEY, None)
+        self.assertIn(self.PREFIX, self.obj._stack)
+
+    def test_callback_add_first_time_key_added(self):
+        self.obj.add(self.PREFIX, self.KEY, None)
+        self.assertIn(self.KEY, self.obj._stack[self.PREFIX])
+
+    def test_callback_add_first_time_callback_added(self):
+        self.obj.add(self.PREFIX, self.KEY, self.callback_mock)
+        self.assertEqual(self.callback_mock,
+                         self.obj._stack[self.PREFIX][self.KEY][0][self.CALLBACK])
+
+    def test_callback_add_oneshot_default_is_true(self):
+        self.obj.add(self.PREFIX, self.KEY, None)
+        self.assertTrue(self.obj._stack[self.PREFIX][self.KEY][0][self.ONE_SHOT])
+
+    def test_callback_add_oneshot_is_false(self):
+        self.obj.add(self.PREFIX, self.KEY, None, False)
+        self.assertFalse(self.obj._stack[self.PREFIX][self.KEY][0][self.ONE_SHOT])
+
+    def test_callback_add_only_caller_default_is_false(self):
+        self.obj.add(self.PREFIX, self.KEY, None)
+        self.assertFalse(self.obj._stack[self.PREFIX][self.KEY][0][self.ONLY_CALLER])
+
+    def test_callback_add_only_caller_true(self):
+        self.obj.add(self.PREFIX, self.KEY, None, only_caller=True)
+        self.assertTrue(self.obj._stack[self.PREFIX][self.KEY][0][self.ONLY_CALLER])
+
+    def test_callback_add_returns_prefix_value_and_key(self):
+        self.assertEqual(self.obj.add(self.PREFIX, self.KEY, None),
+                         (self.PREFIX, self.KEY))
+
+    def test_callback_add_duplicate_callback(self):
+        self.obj.add(self.PREFIX, self.KEY, None)
+        with mock.patch('pika.callback.LOGGER', spec=logging.Logger) as logger:
+            logger.warning = mock.Mock()
+            self.obj.add(self.PREFIX, self.KEY, None)
+            DUPLICATE_WARNING = callback.CallbackManager.DUPLICATE_WARNING
+            logger.warning.assert_called_once_with(DUPLICATE_WARNING,
+                                                   self.PREFIX, self.KEY)
+
+    def test_callback_add_duplicate_callback_returns_prefix_value_and_key(self):
+        self.obj.add(self.PREFIX, self.KEY, None)
+        self.assertEqual(self.obj.add(self.PREFIX, self.KEY, None),
+                         (self.PREFIX, self.KEY))
+
+    def test_callback_clear(self):
+        self.obj.add(self.PREFIX, self.KEY, None)
+        self.obj.clear()
+        self.assertDictEqual(self.obj._stack, dict())
+
+    def test_callback_cleanup_removes_prefix(self):
+        OTHER_PREFIX = 'Foo'
+        self.obj.add(self.PREFIX, self.KEY, None)
+        self.obj.add(OTHER_PREFIX, 'Bar', None)
+        self.obj.cleanup(self.PREFIX)
+        self.assertNotIn(self.PREFIX, self.obj._stack)
+
+    def test_callback_cleanup_keeps_other_prefix(self):
+        OTHER_PREFIX = 'Foo'
+        self.obj.add(self.PREFIX, self.KEY, None)
+        self.obj.add(OTHER_PREFIX, 'Bar', None)
+        self.obj.cleanup(self.PREFIX)
+        self.assertIn(OTHER_PREFIX, self.obj._stack)
+
+    def test_callback_pending_none(self):
+        self.assertIsNone(self.obj.pending(self.PREFIX_CLASS, self.KEY))
+
+    def test_callback_pending_one(self):
+        self.obj.add(self.PREFIX, self.KEY, None)
+        self.assertEqual(self.obj.pending(self.PREFIX_CLASS, self.KEY), 1)
+
+    def test_callback_pending_two(self):
+        self.obj.add(self.PREFIX, self.KEY, None)
+        self.obj.add(self.PREFIX, self.KEY, lambda x: True)
+        self.assertEqual(self.obj.pending(self.PREFIX_CLASS, self.KEY), 2)
+
+    def test_callback_process_false(self):
+        self.assertFalse(self.obj.process(self.PREFIX_CLASS, self.KEY, self))
+
+    def test_callback_process_true(self):
+        self.obj.add(self.PREFIX, self.KEY, self.callback_mock)
+        self.assertTrue(self.obj.process(self.PREFIX_CLASS, self.KEY, self))
+
+    def test_callback_process_mock_called(self):
+        args = (1, None, 'Hi')
+        self.obj.add(self.PREFIX, self.KEY, self.callback_mock)
+        self.obj.process(self.PREFIX, self.KEY, self, args)
+        self.callback_mock.assert_called_once_with(args)
+
+    def test_callback_process_one_shot_removed(self):
+        args = (1, None, 'Hi')
+        self.obj.add(self.PREFIX, self.KEY, self.callback_mock)
+        self.obj.process(self.PREFIX, self.KEY, self, args)
+        self.assertNotIn(self.PREFIX, self.obj._stack)
+
+    def test_callback_process_non_one_shot_prefix_not_removed(self):
+        self.obj.add(self.PREFIX, self.KEY, self.callback_mock, one_shot=False)
+        self.obj.process(self.PREFIX, self.KEY, self)
+        self.assertIn(self.PREFIX, self.obj._stack)
+
+    def test_callback_process_non_one_shot_key_not_removed(self):
+        self.obj.add(self.PREFIX, self.KEY, self.callback_mock, one_shot=False)
+        self.obj.process(self.PREFIX, self.KEY, self)
+        self.assertIn(self.KEY, self.obj._stack[self.PREFIX])
+
+    def test_callback_process_non_one_shot_callback_not_removed(self):
+        self.obj.add(self.PREFIX, self.KEY, self.callback_mock, one_shot=False)
+        self.obj.process(self.PREFIX, self.KEY, self)
+        self.assertEqual(self.obj._stack[self.PREFIX][self.KEY][0][self.CALLBACK],
+                         self.callback_mock)
+
+    def test_callback_process_only_caller_fails(self):
+        self.obj.add(self.PREFIX_CLASS, self.KEY, self.callback_mock,
+                only_caller=self.mock_caller)
+        self.obj.process(self.PREFIX_CLASS, self.KEY, self)
+        self.assertFalse(self.callback_mock.called)
+
+    def test_callback_process_only_caller_fails_no_removal(self):
+        self.obj.add(self.PREFIX_CLASS, self.KEY, self.callback_mock,
+                     only_caller=self.mock_caller)
+        self.obj.process(self.PREFIX_CLASS, self.KEY, self)
+        self.assertEqual(self.obj._stack[self.PREFIX][self.KEY][0][self.CALLBACK],
+                         self.callback_mock)
+
+    def test_callback_remove_with_no_callbacks_pending(self):
+        self.obj = callback.CallbackManager()
+        self.assertFalse(self.obj.remove(self.PREFIX, self.KEY, self.callback_mock))
+
+    def test_callback_remove_with_callback_true(self):
+        self.obj.add(self.PREFIX_CLASS, self.KEY, self.callback_mock)
+        self.assertTrue(self.obj.remove(self.PREFIX, self.KEY, self.callback_mock))
+
+    def test_callback_remove_with_callback_false(self):
+        self.obj.add(self.PREFIX_CLASS, self.KEY, None)
+        self.assertTrue(self.obj.remove(self.PREFIX, self.KEY, self.callback_mock))
+
+    def test_callback_remove_with_callback_true_empty_stack(self):
+        self.obj.add(self.PREFIX_CLASS, self.KEY, self.callback_mock)
+        self.obj.remove(prefix=self.PREFIX, key=self.KEY,
+                        callback_value=self.callback_mock)
+        self.assertDictEqual(self.obj._stack, dict())
+
+    def test_callback_remove_with_callback_true_non_empty_stack(self):
+        self.obj.add(self.PREFIX_CLASS, self.KEY, self.callback_mock)
+        self.obj.add(self.PREFIX_CLASS, self.KEY, self.mock_caller)
+        self.obj.remove(self.PREFIX, self.KEY, self.callback_mock)
+        self.assertEqual(self.mock_caller,
+                         self.obj._stack[self.PREFIX][self.KEY][0][self.CALLBACK])
+
+    def test_callback_remove_prefix_key_with_other_key_prefix_remains(self):
+        OTHER_KEY = 'Other Key'
+        self.obj.add(self.PREFIX_CLASS, self.KEY, self.callback_mock)
+        self.obj.add(self.PREFIX_CLASS, OTHER_KEY, self.mock_caller)
+        self.obj.remove(self.PREFIX, self.KEY, self.callback_mock)
+        self.assertIn(self.PREFIX, self.obj._stack)
+
+    def test_callback_remove_prefix_key_with_other_key_remains(self):
+        OTHER_KEY = 'Other Key'
+        self.obj.add(self.PREFIX_CLASS, self.KEY, self.callback_mock)
+        self.obj.add(prefix=self.PREFIX_CLASS, key=OTHER_KEY,
+                     callback=self.mock_caller)
+        self.obj.remove(self.PREFIX, self.KEY)
+        self.assertIn(OTHER_KEY, self.obj._stack[self.PREFIX])
+
+    def test_callback_remove_prefix_key_with_other_key_callback_remains(self):
+        OTHER_KEY = 'Other Key'
+        self.obj.add(self.PREFIX_CLASS, self.KEY, self.callback_mock)
+        self.obj.add(self.PREFIX_CLASS, OTHER_KEY, self.mock_caller)
+        self.obj.remove(self.PREFIX, self.KEY)
+        self.assertEqual(self.mock_caller,
+                         self.obj._stack[self.PREFIX][OTHER_KEY][0][self.CALLBACK])
+
+    def test_callback_remove_prefix_key(self):
+        self.obj.add(self.PREFIX_CLASS, self.KEY, self.callback_mock)
+        self.obj.remove(self.PREFIX, self.KEY)
+        self.assertNotIn(self.PREFIX, self.obj._stack)
+
+    def test_should_process_callback_true(self):
+        self.obj.add(self.PREFIX_CLASS, self.KEY, self.callback_mock)
+        value = self.obj._callback_dict(self.callback_mock, False, None)
+        self.assertTrue(self.obj._should_process_callback(value,
+                                                          self.mock_caller))
+
+    def test_should_process_callback_false_only_caller_failure(self):
+        self.obj.add(self.PREFIX_CLASS, self.KEY, self.callback_mock)
+        value = self.obj._callback_dict(self.callback_mock, False, self)
+        self.assertTrue(self.obj._should_process_callback(value,
+                                                          self.mock_caller))
+
+    def test_should_process_callback_false_only_caller_failure(self):
+        self.obj.add(self.PREFIX_CLASS, self.KEY, self.callback_mock)
+        value = self.obj._callback_dict(self.callback_mock, False,
+                                        self.mock_caller)
+        self.assertTrue(self.obj._should_process_callback(value,
+                                                          self.mock_caller))
+
+    def test_callback_dict(self):
+        expectation =  {self.CALLBACK: self.callback_mock,
+                        self.ONE_SHOT: True,
+                        self.ONLY_CALLER: self.mock_caller}
+        self.assertDictEqual(self.obj._callback_dict(self.callback_mock,
+                                                     True, self.mock_caller),
+                             expectation)
+
