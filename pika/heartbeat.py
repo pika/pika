b0ec60a7775e86c148d2f4619674962f0d49aa44
Gavin M. Roy <gmr@meetme.com>
2012-10-01 23:23:46 -0400

    Refactor HeartbeatChecker, fixing issue #175
    - Add full test coverage
    - Now counts frames received
    - Will keep track of bytes sent/received and heartbeats sent/received
    - No longer will try and reconnect the connection, will just call connection close

--- pika/heartbeat.py
+++ pika/heartbeat.py
@@ -3,13 +3,11 @@
 # For copyright and licensing please refer to COPYING.
 #
 # ***** END LICENSE BLOCK *****
-"""
-Class that handles heartbeat communication
-
-"""
-import time
+import logging
 
 from pika import frame
+
+LOGGER = logging.getLogger(__name__)
 
 
 class HeartbeatChecker(object):
@@ -17,129 +15,67 @@ class HeartbeatChecker(object):
     intervals.
 
     """
+    MAX_MISSED_HEARTBEATS = 2
     _CONNECTION_FORCED = 320
-    _MAX_MISSED_HEARTBEATS = 2
     _STALE_CONNECTION = "Too Many Missed Heartbeats, No reply in %i seconds"
 
-    def __init__(self, connection, interval):
+    def __init__(self, connection, interval, idle_count=MAX_MISSED_HEARTBEATS):
         """Create a heartbeat on connection sending a heartbeat frame every
         interval seconds.
 
         :param pika.connection.Connection: Connection object
         :param int interval: Heartbeat check interval
+        :param int idle_count: Number of heartbeat intervals missed until the
+                               connection is considered idle and disconnects
 
         """
-        # We need to reference our connection object to close a connection
         self._connection = connection
         self._interval = interval
+        self._max_idle_count = idle_count
 
         # Initialize counters
-        self._missed = 0
-        self._received = 0
-        self._sent = 0
+        self._bytes_received = 0
+        self._bytes_sent = 0
+        self._heartbeat_frames_received = 0
+        self._heartbeat_frames_sent = 0
+        self._idle_byte_intervals = 0
+        self._idle_heartbeat_intervals = 0
+        self._last_interval_frames_received = 0
 
         # Setup the timer to fire in _interval seconds
         self._setup_timer()
 
-    def _close_connection(self):
-        """Close the connection with the AMQP Connection-Forced value."""
-        duration = self._missed * self._interval
-        self._connection.close(HeartbeatChecker._CONNECTION_FORCED,
-                               HeartbeatChecker._STALE_CONNECTION % duration)
-
-    def _reset_connection(self):
-        """Tell the connection to re-connect"""
-        self._connection.force_reconnect()
-
-    def _connection_bytes_received(self):
-        """Return the number of bytes received by the connection bytes object.
-
-        :returns: int
-
-        """
-        return self._connection.bytes_received
-
-    def _in_use(self):
+    @property
+    def active(self):
         """Return True if the connection's heartbeat attribute is set to this
         instance.
 
-        :returns: True
+        :rtype True
 
         """
         return self._connection.heartbeat is self
 
-    def _new_heartbeat_frame(self):  #pragma: no cover
-        """Return a new heartbeat frame.
+    @property
+    def bytes_received_on_connection(self):
+        """Return the number of bytes received by the connection bytes object.
 
-        :returns: pika.frame.Heartbeat
-
-        """
-        return frame.Heartbeat()
-
-    def _missed_heartbeat_responses(self):
-        """Check to see if the received byte count matches the connection
-        object received byte count. If the bytes are equal, there has not been
-        a heartbeat sent since the last check.
+        :rtype int
 
         """
-        if self._received == self._connection_bytes_received():
-            self._missed += 1
-        else:
-            # The server has said something. Reset our count.
-            self._missed = 0
+        return self._connection.bytes_received
 
-        # Return the missed heartbeat response count
-        return self._missed
-
-    def _send_heartbeat_frame(self):
-        """Send a heartbeat frame on the connection.
+    @property
+    def connection_is_idle(self):
+        """Returns true if the byte count hasn't changed in enough intervals
+        to trip the max idle threshold.
 
         """
-        self._connection._send_frame(self._new_heartbeat_frame())
+        return self._idle_byte_intervals >= self._max_idle_count
 
-    def _setup_timer(self):
-        """Use the connection objects delayed_call function which is
-        implemented by the Adapter for calling the check_heartbeats function
-        every interval seconds.
-
-        """
-        self._connection.add_timeout(time.time()+self._interval, self.send_and_check)
-
-    def _should_send_heartbeat_frame(self):
-        """Returns True if the amount of bytes recorded the last time the
-        timer fired still matches the number of bytes the connection has
-        sent.
-
-        :returns: True
-
-        """
-        return self._sent == self._connection.bytes_sent
-
-    def _start_timer(self):
-        """If the connection still has this object set for heartbeats, add a
-        new timer.
-
-        """
-        if self._in_use():
-            self._setup_timer()
-
-    def _too_many_missed_heartbeats(self):
-        """Return if the number of missed heartbeats exceeds the maximum amount
-        of missed heartbeats.
-
-        :returns: True
-
-        """
-        return (self._missed_heartbeat_responses() >=
-                HeartbeatChecker._MAX_MISSED_HEARTBEATS)
-
-    def _update_byte_counts(self):
-        """Update the internal byte counters from the current values of the
-        connection object.
-
-        """
-        self._sent = self._connection.bytes_sent
-        self._received = self._connection.bytes_received
+    def received(self):
+        """Called when a heartbeat is received"""
+        LOGGER.debug('Received heartbeat frame')
+        self._heartbeat_frames_received += 1
 
     def send_and_check(self):
         """Invoked by a timer to send a heartbeat when we need to, check to see
@@ -147,18 +83,107 @@ class HeartbeatChecker(object):
         been idle too long.
 
         """
-        # If too many heartbeats have been missed, close & reset the connection
-        if self._too_many_missed_heartbeats():
-            self._close_connection()
-            self._reset_connection()
-            return
+        LOGGER.debug('Received %i heartbeat frames, sent %i',
+                     self._heartbeat_frames_received,
+                     self._heartbeat_frames_sent)
 
-        # If there have been no bytes received since the last check
-        if self._should_send_heartbeat_frame():
-            self._send_heartbeat_frame()
+        # If too many heartbeats have been missed, close the connection
+        if self.too_many_missed_heartbeats or self.connection_is_idle:
+            return self._close_connection()
 
-        # Update the byte counts for the next check
-        self._update_byte_counts()
+        # Connection has not received any data, increment the counter
+        if not self._has_received_data:
+            self._idle_byte_intervals += 1
+
+        # Connection has not received any heartbeats, increment the counter
+        if not self._has_received_frames:
+            self._idle_heartbeat_intervals += 1
+
+        # Update the counters of bytes sent/received and the frames received
+        self._update_counters()
+
+        # Send a heartbeat frame
+        self._send_heartbeat_frame()
 
         # Update the timer to fire again
         self._start_timer()
+
+    @property
+    def too_many_missed_heartbeats(self):
+        """Return if the number of missed heartbeats exceeds the maximum amount
+        of missed heartbeats.
+
+        :rtype True
+
+        """
+        return self._idle_heartbeat_intervals >= self._max_idle_count
+
+    def _close_connection(self):
+        """Close the connection with the AMQP Connection-Forced value."""
+        LOGGER.info('Connection has missed too many heartbeats or the '
+                    'connection is idle')
+        LOGGER.debug('%i missed frames, %i stale byte intervals',
+                     self._idle_heartbeat_intervals, self._idle_byte_intervals)
+        duration = self._max_idle_count * self._interval
+        self._connection.close(HeartbeatChecker._CONNECTION_FORCED,
+                               HeartbeatChecker._STALE_CONNECTION % duration)
+
+    @property
+    def _has_received_data(self):
+        """Returns True if the connection has received data on the connection.
+
+        :rtype: bool
+
+        """
+        return not self._bytes_received == self.bytes_received_on_connection
+
+    @property
+    def _has_received_frames(self):
+        """Returns True if the connection has received heartbeat frames.
+
+        :rtype: bool
+
+        """
+        return not (self._heartbeat_frames_received ==
+                    self._last_interval_frames_received)
+
+    def _new_heartbeat_frame(self):
+        """Return a new heartbeat frame.
+
+        :rtype pika.frame.Heartbeat
+
+        """
+        return frame.Heartbeat()
+
+    def _send_heartbeat_frame(self):
+        """Send a heartbeat frame on the connection.
+
+        """
+        LOGGER.debug('Sending heartbeat frame')
+        self._connection._send_frame(self._new_heartbeat_frame())
+        self._heartbeat_frames_sent += 1
+
+    def _setup_timer(self):
+        """Use the connection objects delayed_call function which is
+        implemented by the Adapter for calling the check_heartbeats function
+        every interval seconds.
+
+        """
+        self._connection.add_timeout(self._interval, self.send_and_check)
+
+    def _start_timer(self):
+        """If the connection still has this object set for heartbeats, add a
+        new timer.
+
+        """
+        if self.active:
+            self._setup_timer()
+
+    def _update_counters(self):
+        """Update the internal counters for bytes sent and received and the
+        number of frames received
+
+        """
+        self._bytes_sent = self._connection.bytes_sent
+        self._bytes_received = self._connection.bytes_received
+        self._last_interval_frames_received = self._heartbeat_frames_received
---
+++ tests/heartbeat_tests.py
@@ -0,0 +1,226 @@
+"""
+Tests for pika.heartbeat
+
+"""
+import mock
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+from pika import connection
+from pika import frame
+from pika import heartbeat
+
+class HeartbeatTests(unittest.TestCase):
+
+    INTERVAL = 5
+
+    def setUp(self):
+        self.mock_conn = mock.Mock(spec=connection.Connection)
+        self.mock_conn.bytes_received = 100
+        self.mock_conn.bytes_sent = 100
+        self.mock_conn.heartbeat = mock.Mock(spec=heartbeat.HeartbeatChecker)
+        self.obj = heartbeat.HeartbeatChecker(self.mock_conn, self.INTERVAL)
+
+    def tearDown(self):
+        del self.obj
+        del self.mock_conn
+
+    def test_default_initialization_max_idle_count(self):
+        self.assertEqual(self.obj._max_idle_count,
+                         self.obj.MAX_MISSED_HEARTBEATS)
+
+    def test_constructor_assignment_connection(self):
+        self.assertEqual(self.obj._connection, self.mock_conn)
+
+    def test_constructor_assignment_heartbeat_interval(self):
+        self.assertEqual(self.obj._interval, self.INTERVAL)
+
+    def test_constructor_initial_bytes_received(self):
+        self.assertEqual(self.obj._bytes_received, 0)
+
+    def test_constructor_initial_bytes_sent(self):
+        self.assertEqual(self.obj._bytes_received, 0)
+
+    def test_constructor_initial_heartbeat_frames_received(self):
+        self.assertEqual(self.obj._heartbeat_frames_received, 0)
+
+    def test_constructor_initial_heartbeat_frames_sent(self):
+        self.assertEqual(self.obj._heartbeat_frames_sent, 0)
+
+    def test_constructor_initial_idle_byte_intervals(self):
+        self.assertEqual(self.obj._idle_byte_intervals, 0)
+
+    def test_constructor_initial_idle_heartbeat_intervals(self):
+        self.assertEqual(self.obj._idle_heartbeat_intervals, 0)
+
+    def test_constructor_initial_last_interval_frames_received(self):
+        self.assertEqual(self.obj._last_interval_frames_received, 0)
+
+    @mock.patch('pika.heartbeat.HeartbeatChecker._setup_timer')
+    def test_constructor_called_setup_timer(self, timer):
+        obj = heartbeat.HeartbeatChecker(self.mock_conn, self.INTERVAL)
+        timer.assert_called_once_with()
+
+    def test_active_true(self):
+        self.mock_conn.heartbeat = self.obj
+        self.assertTrue(self.obj.active)
+
+    def test_active_false(self):
+        self.mock_conn.heartbeat = mock.Mock()
+        self.assertFalse(self.obj.active)
+
+    def test_bytes_received_on_connection(self):
+        self.mock_conn.bytes_received = 128
+        self.assertEqual(self.obj.bytes_received_on_connection, 128)
+
+    def test_connection_is_idle_false(self):
+        self.assertFalse(self.obj.connection_is_idle)
+
+    def test_connection_is_idle_true(self):
+        self.obj._idle_byte_intervals = self.INTERVAL
+        self.assertTrue(self.obj.connection_is_idle)
+
+    def test_received(self):
+        self.obj.received()
+        self.assertTrue(self.obj._heartbeat_frames_received, 1)
+
+    @mock.patch('pika.heartbeat.HeartbeatChecker._close_connection')
+    def test_send_and_check_not_closed(self, close_connection):
+        obj = heartbeat.HeartbeatChecker(self.mock_conn, self.INTERVAL)
+        obj.send_and_check()
+        close_connection.assert_not_called()
+
+    @mock.patch('pika.heartbeat.HeartbeatChecker._close_connection')
+    def test_send_and_check_missed_heartbeats(self, close_connection):
+        obj = heartbeat.HeartbeatChecker(self.mock_conn, self.INTERVAL)
+        obj._idle_heartbeat_intervals = self.INTERVAL
+        obj.send_and_check()
+        close_connection.assert_called_once_with()
+
+    @mock.patch('pika.heartbeat.HeartbeatChecker._close_connection')
+    def test_send_and_check_missed_bytes(self, close_connection):
+        obj = heartbeat.HeartbeatChecker(self.mock_conn, self.INTERVAL)
+        obj._idle_byte_intervals = self.INTERVAL
+        obj.send_and_check()
+        close_connection.assert_called_once_with()
+
+    def test_send_and_check_increment_no_bytes(self):
+        self.mock_conn.bytes_received = 100
+        self.obj._bytes_received = 100
+        self.obj.send_and_check()
+        self.assertEqual(self.obj._idle_byte_intervals, 1)
+
+    def test_send_and_check_increment_bytes(self):
+        self.mock_conn.bytes_received = 100
+        self.obj._bytes_received = 128
+        self.obj.send_and_check()
+        self.assertEqual(self.obj._idle_byte_intervals, 0)
+
+    def test_send_and_check_increment_no_heartbeats(self):
+        self.obj._heartbeat_frames_received = 1
+        self.obj._last_interval_frames_received = 1
+        self.obj.send_and_check()
+        self.assertEqual(self.obj._idle_heartbeat_intervals, 1)
+
+    def test_send_and_check_increment_heartbeats(self):
+        self.obj._heartbeat_frames_received = 1
+        self.obj._last_interval_frames_received = 0
+        self.obj.send_and_check()
+        self.assertEqual(self.obj._idle_heartbeat_intervals, 0)
+
+    @mock.patch('pika.heartbeat.HeartbeatChecker._update_counters')
+    def test_send_and_check_update_counters(self, update_counters):
+        obj = heartbeat.HeartbeatChecker(self.mock_conn, self.INTERVAL)
+        obj.send_and_check()
+        update_counters.assert_called_once_with()
+
+    @mock.patch('pika.heartbeat.HeartbeatChecker._send_heartbeat_frame')
+    def test_send_and_check_send_heartbeat_frame(self, send_heartbeat_frame):
+        obj = heartbeat.HeartbeatChecker(self.mock_conn, self.INTERVAL)
+        obj.send_and_check()
+        send_heartbeat_frame.assert_called_once_with()
+
+    @mock.patch('pika.heartbeat.HeartbeatChecker._start_timer')
+    def test_send_and_check_start_timer(self, start_timer):
+        obj = heartbeat.HeartbeatChecker(self.mock_conn, self.INTERVAL)
+        obj.send_and_check()
+        start_timer.assert_called_once_with()
+
+    def test_too_many_missed_heartbeats_false(self):
+        self.assertFalse(self.obj.too_many_missed_heartbeats)
+
+    def test_too_many_missed_heartbeats_true(self):
+        self.obj._idle_heartbeat_intervals = self.INTERVAL
+        self.assertTrue(self.obj.too_many_missed_heartbeats)
+
+    def test_connection_close(self):
+        self.obj._idle_byte_intervals = 3
+        self.obj._idle_heartbeat_intervals = 4
+        self.obj._close_connection()
+        self.mock_conn.close.assert_called_once_with(self.obj._CONNECTION_FORCED,
+                                                     self.obj._STALE_CONNECTION %
+                                                     (self.obj._max_idle_count *
+                                                      self.obj._interval))
+
+    def test_has_received_data_false(self):
+        self.obj._bytes_received = 100
+        self.assertFalse(self.obj._has_received_data)
+
+    def test_has_received_data_true(self):
+        self.mock_conn.bytes_received = 128
+        self.obj._bytes_received = 100
+        self.assertTrue(self.obj._has_received_data)
+
+    def test_has_received_frames_false(self):
+        self.obj._heartbeat_frames_received = 100
+        self.obj._last_interval_frames_received = 100
+        self.assertFalse(self.obj._has_received_frames)
+
+    def test_has_received_frames_true(self):
+        self.obj._heartbeat_frames_received = 128
+        self.obj._last_interval_frames_received = 100
+        self.assertTrue(self.obj._has_received_frames)
+
+    def test_new_heartbeat_frame(self):
+        self.assertIsInstance(self.obj._new_heartbeat_frame(), frame.Heartbeat)
+
+    def test_send_heartbeat_send_frame_called(self):
+        self.obj._send_heartbeat_frame()
+        self.mock_conn._send_frame.assert_called_once()
+
+    def test_send_heartbeat_counter_incremented(self):
+        self.obj._send_heartbeat_frame()
+        self.assertEqual(self.obj._heartbeat_frames_sent, 1)
+
+    def test_setup_timer_called(self):
+        self.obj._setup_timer()
+        self.mock_conn.add_timeout.called_once_with(self.INTERVAL,
+                                                    self.obj.send_and_check)
+
+    @mock.patch('pika.heartbeat.HeartbeatChecker._setup_timer')
+    def test_start_timer_not_active(self, setup_timer):
+        self.obj._start_timer()
+        setup_timer.assert_not_called()
+
+    @mock.patch('pika.heartbeat.HeartbeatChecker._setup_timer')
+    def test_start_timer_active(self, setup_timer):
+        self.mock_conn.heartbeat = self.obj
+        self.obj._start_timer()
+        self.assertTrue(setup_timer.called)
+
+    def test_update_counters_bytes_received(self):
+        self.mock_conn.bytes_received = 256
+        self.obj._update_counters()
+        self.assertEqual(self.obj._bytes_received, 256)
+
+    def test_update_counters_bytes_sent(self):
+        self.mock_conn.bytes_sent = 256
+        self.obj._update_counters()
+        self.assertEqual(self.obj._bytes_sent, 256)
+
+    def test_update_counters_heartbeat_frames(self):
+        self.obj._heartbeat_frames_received = 100
+        self.obj._update_counters()
+        self.assertEqual(self.obj._last_interval_frames_received, 100)
