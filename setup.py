08fc3b0569fd1451491572557481f8ae1b69e3f9
Gavin M. Roy <gmr@meetme.com>
2013-04-07 18:37:13 -0400

    Bump the pre-release version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ long_description = ('Pika is a pure-Python implementation of the AMQP 0-9-1 '
                     'with other AMQP 0-9-1 brokers.')
 
 setup(name='pika',
-      version='0.9.13p0',
+      version='0.9.13p1',
       description='Pika Python AMQP Client Library',
       long_description=long_description,
       author='Tony Garnock-Jones',
@@ -37,8 +37,8 @@ setup(name='pika',
       packages=['pika', 'pika.adapters'],
       license='MPL v1.1 and GPL v2.0 or newer',
       install_requires=requirements,
-      extras_require={'tornado_connection': ['tornado'],
-                      'twisted_connection': ['twisted']},
+      extras_require={'tornado': ['tornado'],
+                      'twisted': ['twisted']},
       tests_require=tests_require,
       test_suite='nose.collector',
       classifiers=[
