d03b385b5d23c321ee1d4bd2020be1452e8c1cab
Gavin M. Roy <gmr@meetme.com>
2013-05-15 12:18:14 -0400

    Remove Python 2.4 support monkey patch and bump rev

--- pika/__init__.py
+++ pika/__init__.py
@@ -3,7 +3,7 @@
 # For copyright and licensing please refer to COPYING.
 #
 # ***** END LICENSE BLOCK *****
-__version__ = '0.9.13p1'
+__version__ = '0.9.13p2'
 
 from pika.connection import ConnectionParameters
 from pika.connection import URLParameters
@@ -14,13 +14,3 @@ from pika.adapters.base_connection import BaseConnection
 from pika.adapters.asyncore_connection import AsyncoreConnection
 from pika.adapters.blocking_connection import BlockingConnection
 from pika.adapters.select_connection import SelectConnection
-
-# Python 2.4 support: add struct.unpack_from if it's missing.
-try:
-    import struct
-    getattr(struct, "unpack_from")
-except AttributeError:
-    def _unpack_from(fmt, buf, offset=0):
-        slice = buffer(buf, offset, struct.calcsize(fmt))
-        return struct.unpack(fmt, slice)
-    struct.unpack_from = _unpack_from
