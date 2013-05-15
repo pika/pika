4b7f182fd9485afe8bda1421e18bb7b41e00156c
Gavin M. Roy <gmr@meetme.com>
2013-05-15 12:19:12 -0400

    Don't fail tornado tests if tornado is not installed, just skip them

--- tests/tornado_tests.py
+++ tests/tornado_tests.py
@@ -2,18 +2,27 @@
 Tests for pika.adapters.tornado_connection
 
 """
-from tornado import ioloop
+try:
+    from tornado import ioloop
+except ImportError:
+    ioloop = None
+
 import mock
+
 try:
     import unittest2 as unittest
 except ImportError:
     import unittest
 
-from pika.adapters import tornado_connection
+try:
+    from pika.adapters import tornado_connection
+except ImportError:
+    tornado_connection = None
 
 
 class TornadoConnectionTests(unittest.TestCase):
 
+    @unittest.skipIf(ioloop is None, 'requires Tornado')
     @mock.patch('pika.adapters.base_connection.BaseConnection.__init__')
     def test_tornado_connection_call_parent(self, mock_init):
         obj = tornado_connection.TornadoConnection()
