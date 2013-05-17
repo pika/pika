8b69e5afadff4b887f60a168ab2cae8d5ae19322
Mark Unsworth <mark@knows.is>
2013-05-17 17:14:10 +0100

    calls self._adapter_disconnect() instead of self.disconnect() which doesn't actually exist #294

--- pika/adapters/blocking_connection.py
+++ pika/adapters/blocking_connection.py
@@ -315,7 +315,7 @@ class BlockingConnection(base_connection.BaseConnection):
 
     def _handle_disconnect(self):
         """Called internally when the socket is disconnected already"""
-        self.disconnect()
+        self._adapter_disconnect()
         self._on_connection_closed(None, True)
 
     def _handle_read(self):
