2c908863a16461e361346dcc73f25ffd626d9996
Raphaël De Giusti <raphael.degiusti@gmail.com>
2013-08-05 11:07:03 +0200

    Update scripts to build latest rpm

--- scripts/build-rpm.sh
+++ scripts/build-rpm.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
 NAME="python-pika"
-VERSION="0.9.13"
-VERSION="0"
+SPEC_FILE_NAME="python-pika"
+PLATFORMS="epel-6-i386 fedora-18-i386 fedora-19-i386"
+TAR_CONTENT="pika setup.py docs examples integration.cfg LICENSE-GPL-2.0 LICENSE-MPL-Pika README.md tests utils"
 
-tar -cvzf $HOME/rpmbuild/SOURCES/${NAME}-${VERSION}-${RELEASE}.tar.gz * \
---exclude .git
+cd `dirname $0`
+cd ..
 
+# Set version information
+. scripts/build-pkg-functions
+set_version $1 $2
 
+tar -cvzf $HOME/rpmbuild/SOURCES/${NAME}-${VERSION}-${RELEASE}.tar.gz ${TAR_CONTENT}
+
+sed "s/#VERSION#/${VERSION}/g" ${SPEC_FILE_NAME}.spec.template > ${SPEC_FILE_NAME}.spec
+sed -i "s/#RELEASE#/${RELEASE}/g" ${SPEC_FILE_NAME}.spec
+sed -i "s/#COMMIT#/${COMMIT}/g" ${SPEC_FILE_NAME}.spec
 rpmbuild -ba ${NAME}.spec
 
+for platform in $PLATFORMS
+do
+    /usr/bin/mock -r ${platform} --rebuild $HOME/rpmbuild/SRPMS/${NAME}-${VERSION}-${RELEASE}*.src.rpm
+done
