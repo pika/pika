223440d830110a7bcc0e798e35b4555e0fbe7920
Raphaël De Giusti <raphael.degiusti@gmail.com>
2013-08-05 14:03:20 +0200

    Updated build deb scripts to build latest

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -2,14 +2,27 @@
 
 NAME="python-pika"
 TMP_DIR=/tmp/python-pika
+DEBEMAIL="builder-deb@comodit.com"
+DEBFULLNAME="ComodIT"
 
 cd `dirname $0`
 cd ..
 
 # Set version information
-. scripts/build-pkg-functions
-# set_version $1 $2
-# generate_version_file $VERSION $RELEASE
+VERSION=`git describe --tags  | awk -F"-" '{print $1}'`
+RELEASE=`git describe --tags  | awk -F"-" '{print $2}'`
+COMMIT=`git describe --tags  | awk -F"-" '{print $4}'`
+MESSAGE="Release $VERSION-$RELEASE-$COMMIT"
+
+echo $MESSAGE
+
+export DEBEMAIL
+export DEBFULLNAME
+
+debchange --newversion $VERSION-$RELEASE "$MESSAGE"
+
+unset DEBEMAIL
+unset DEBFULLNAME
 
 # Build package
 DIST_DIR=${TMP_DIR}/dist
