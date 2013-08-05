ec1a731d7cea13fb7442e92e9bd82ed5c9903bf0
Raphaël De Giusti <raphael.degiusti@gmail.com>
2013-08-05 14:54:21 +0200

    Fixed some deb packaging issues

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -12,7 +12,7 @@ cd ..
 # Set version information
 VERSION=`git describe --tags  | awk -F"-" '{print $1}'`
 RELEASE=`git describe --tags  | awk -F"-" '{print $2}'`
-COMMIT=`git describe --tags  | awk -F"-" '{print $4}'`
+COMMIT=`git describe --tags  | awk -F"-" '{print $3}'`
 MESSAGE="Release $VERSION-$RELEASE-$COMMIT"
 
 echo $MESSAGE
@@ -28,7 +28,7 @@ unset DEBFULLNAME
 # Build package
 DIST_DIR=${TMP_DIR}/dist
 python setup.py sdist --dist-dir=${DIST_DIR}
-mv ${DIST_DIR}/$NAME-$VERSION-$RELEASE.tar.gz $NAME\_$VERSION.$RELEASE.orig.tar.gz
+mv ${DIST_DIR}/*.tar.gz ../$NAME\_$VERSION.orig.tar.gz
 dpkg-buildpackage -i -I -rfakeroot
 
 # Clean-up
