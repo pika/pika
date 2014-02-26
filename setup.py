e3cf2622660fc77df9c19e3b455dde1162ab383f
Sebastien CAPS <sebastien.caps@guardis.com>
2014-02-26 13:33:08 +0100

    Fixed debian package on python 2.6 platform

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ import platform
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 requirements = list()
 if on_rtd:
-    requirements.append('python >= 2.6')
     requirements.append('tornado')
     requirements.append('twisted')
     requirements.append('pyev')
