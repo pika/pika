1053ebc2eeeaa675a1d9d346383e5f0f91d0b780
Gavin M. Roy <gavinmroy@gmail.com>
2015-04-29 01:04:09 -0400

    Late night bug

--- pika/adapters/base_connection.py
+++ pika/adapters/base_connection.py
@@ -166,7 +166,7 @@ class BaseConnection(connection.Connection):
         """Close the socket cleanly"""
         if self.socket:
             try:
-                if hasattr('socket', self.socket):
+                if hasattr(self.socket, 'socket'):
                     self.socket.socket.shutdown(socket.SHUT_RDWR)
                 else:
                     self.socket.shutdown(socket.SHUT_RDWR)
