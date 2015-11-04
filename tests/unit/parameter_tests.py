0ed4d613fc42b31083660500b2b799e6d1f9cc79
Gavin M. Roy <gavinr@aweber.com>
2015-11-04 17:08:46 -0500

    Line wrap

--- tests/unit/parameter_tests.py
+++ tests/unit/parameter_tests.py
@@ -21,8 +21,9 @@ class ParameterTests(unittest.TestCase):
         self.assertEqual(parameters.locale, "en_US")
 
     def test_urlparameters_accepts_plain_string(self):
-        parameters = pika.URLParameters(
-            "amqp://prtfqpeo:oihdglkhcp0@myserver.mycompany.com:5672/prtfqpeo?locale=en_US")
+        parameters = pika.URLParameters('amqp://prtfqpeo:oihdglkhcp0@myserver.'
+                                        'mycompany.com:5672/prtfqpeo?locale='
+                                        'en_US')
         self.assertEqual(parameters.port, 5672)
         self.assertEqual(parameters.virtual_host, "prtfqpeo")
         self.assertEqual(parameters.credentials.password, "oihdglkhcp0")
@@ -30,8 +31,9 @@ class ParameterTests(unittest.TestCase):
         self.assertEqual(parameters.locale, "en_US")
 
     def test_urlparameters_accepts_unicode_string(self):
-        parameters = pika.URLParameters(
-            u"amqp://prtfqpeo:oihdglkhcp0@myserver.mycompany.com:5672/prtfqpeo?locale=en_US")
+        parameters = pika.URLParameters(u'amqp://prtfqpeo:oihdglkhcp0@myserver'
+                                        u'.mycompany.com:5672/prtfqpeo?locale='
+                                        u'en_US')
         self.assertEqual(parameters.port, 5672)
         self.assertEqual(parameters.virtual_host, "prtfqpeo")
         self.assertEqual(parameters.credentials.password, "oihdglkhcp0")
