777d06341ca1285e3a92861e0f978065ec46544b
Dmitry Orlov <me@mosquito.su>
2016-09-14 20:06:24 +0300

    Add docstrings

--- pika/adapters/asyncio_connection.py
+++ pika/adapters/asyncio_connection.py
@@ -1,5 +1,4 @@
 import asyncio
-import logging
 from functools import partial
 
 from pika.adapters import base_connection
@@ -12,13 +11,37 @@ class IOLoopAdapter:
         self.loop = loop
 
     def add_timeout(self, deadline, callback_method):
+        """Add the callback_method to the EventLoop timer to fire after deadline
+        seconds. Returns a Handle to the timeout.
+
+        :param int deadline: The number of seconds to wait to call callback
+        :param method callback_method: The callback method
+        :rtype: asyncio.Handle
+
+        """
         return self.loop.call_later(deadline, callback_method)
 
     @staticmethod
-    def remove_timeout(handle: asyncio.Handle):
+    def remove_timeout(handle):
+        """
+        Cancel asyncio.Handle
+
+        :type handle: asyncio.Handle
+        :rtype: bool
+        """
         return handle.cancel()
 
     def add_handler(self, fd, cb, event_state):
+        """ Registers the given handler to receive the given events for ``fd``.
+
+        The ``fd`` argument is an integer file descriptor.
+
+        The ``event_state`` argument is a bitwise or of the constants
+        ``base_connection.BaseConnection.READ``, ``base_connection.BaseConnection.WRITE``,
+        and ``base_connection.BaseConnection.ERROR``.
+
+        """
+
         if event_state & base_connection.BaseConnection.READ:
             self.loop.add_reader(
                 fd,
@@ -40,6 +63,8 @@ class IOLoopAdapter:
             )
 
     def remove_handler(self, fd):
+        """ Stop listening for events on ``fd``. """
+
         self.loop.remove_reader(fd)
         self.loop.remove_writer(fd)
 
@@ -47,24 +72,45 @@ class IOLoopAdapter:
         raise NotImplementedError()
 
     def start(self):
-        if loop.is_running():
+        """ Start Event Loop """
+        if self.loop.is_running():
             return
 
         self.loop.run_forever()
 
     def stop(self):
-        if loop.is_closed():
+        """ Stop Event Loop """
+        if self.loop.is_closed():
             return
 
         self.loop.stop()
 
 
 class AsyncioConnection(base_connection.BaseConnection):
+    """ The AsyncioConnection runs on the Asyncio EventLoop.
 
+    :param pika.connection.Parameters parameters: Connection parameters
+    :param on_open_callback: The method to call when the connection is open
+    :type on_open_callback: method
+    :param on_open_error_callback: Method to call if the connection cant be opened
+    :type on_open_error_callback: method
+    :param asyncio.AbstractEventLoop loop: By default asyncio.get_event_loop()
+
+    """
     def __init__(self, parameters=None, on_open_callback=None,
                  on_open_error_callback=None,
                  on_close_callback=None, loop=None):
+        """ Create a new instance of the AsyncioConnection class, connecting
+        to RabbitMQ automatically
 
+        :param pika.connection.Parameters parameters: Connection parameters
+        :param on_open_callback: The method to call when the connection is open
+        :type on_open_callback: method
+        :param on_open_error_callback: Method to call if the connection cant be opened
+        :type on_open_error_callback: method
+        :param asyncio.AbstractEventLoop loop: By default asyncio.get_event_loop()
+
+        """
         self.sleep_counter = 0
         self.loop = loop or asyncio.get_event_loop()
         self.ioloop = IOLoopAdapter(self.loop)
@@ -77,6 +123,12 @@ class AsyncioConnection(base_connection.BaseConnection):
         )
 
     def _adapter_connect(self):
+        """Connect to the remote socket, adding the socket to the EventLoop if
+        connected.
+
+        :rtype: bool
+
+        """
         error = super()._adapter_connect()
 
         if not error:
@@ -89,6 +141,8 @@ class AsyncioConnection(base_connection.BaseConnection):
         return error
 
     def _adapter_disconnect(self):
+        """Disconnect from the RabbitMQ broker"""
+
         if self.socket:
             self.ioloop.remove_handler(
                 self.socket.fileno()
