d9d69e1669900628278584222c07b3b3cf043db8
Luke Bakken <luke@bakken.io>
2017-10-04 16:09:02 -0700

    Revert 8257a8ed

--- pika/tcp_socket_opts.py
+++ pika/tcp_socket_opts.py
@@ -15,7 +15,11 @@ try:
     _SUPPORTED_TCP_OPTIONS['TCP_USER_TIMEOUT'] = socket.TCP_USER_TIMEOUT
 except AttributeError:
     if pika.compat.LINUX_VERSION and pika.compat.LINUX_VERSION >= (2, 6, 37):
-        _SUPPORTED_TCP_OPTIONS['TCP_USER_TIMEOUT'] = 60
+        # NB: this is not the timeout value, but the number corresponding
+        # to the constant in tcp.h
+        # https://github.com/torvalds/linux/blob/master/include/uapi/linux/tcp.h#
+        # #define TCP_USER_TIMEOUT	18	/* How long for loss retry before timeout */
+        _SUPPORTED_TCP_OPTIONS['TCP_USER_TIMEOUT'] = 18
 
 try:
     _SUPPORTED_TCP_OPTIONS['TCP_KEEPIDLE'] = socket.TCP_KEEPIDLE
