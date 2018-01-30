559a6c2ebe7ac99cb6bc91696b82cf2732f89d7d
Hugo <hugovk@users.noreply.github.com>
2018-01-30 15:07:57 +0200

    Rewrite dict creation as literal

--- examples/producer.py
+++ examples/producer.py
@@ -17,13 +17,14 @@ else:
     main_channel.exchange_declare(exchange='com.micex.sten',       exchange_type='direct')
     main_channel.exchange_declare(exchange='com.micex.lasttrades', exchange_type='direct')
 
-tickers = {}
-tickers['MXSE.EQBR.LKOH'] = (1933,1940)
-tickers['MXSE.EQBR.MSNG'] = (1.35,1.45)
-tickers['MXSE.EQBR.SBER'] = (90,92)
-tickers['MXSE.EQNE.GAZP'] = (156,162)
-tickers['MXSE.EQNE.PLZL'] = (1025,1040)
-tickers['MXSE.EQNL.VTBR'] = (0.05,0.06)
+tickers = {'MXSE.EQBR.LKOH': (1933, 1940),
+           'MXSE.EQBR.MSNG': (1.35, 1.45),
+           'MXSE.EQBR.SBER': (90, 92),
+           'MXSE.EQNE.GAZP': (156, 162),
+           'MXSE.EQNE.PLZL': (1025, 1040),
+           'MXSE.EQNL.VTBR': (0.05, 0.06)}
+
+
 def getticker(): return list(tickers.keys())[random.randrange(0,len(tickers)-1)]
 
 _COUNT_ = 10
