03eb2bf8ac224def36686f9e1bf6063997efd7cd
Luke Bakken <lbakken@pivotal.io>
2022-04-27 07:08:03 -0700

    Remove references to Python 2

--- setup.py
+++ setup.py
@@ -41,8 +41,6 @@ setuptools.setup(
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
