d678353e31c32175bcc5f5b6cd73b91c76f63e53
Sasha Rudan <mprinc@gmail.com>
2022-11-05 18:37:16 +0100

    Fixing pika v1.0.0 breaking change (order of parameters) for the `channel.basic_consume` method

--- examples/basic_consumer_threaded.py
+++ examples/basic_consumer_threaded.py
@@ -54,7 +54,7 @@ channel.basic_qos(prefetch_count=1)
 
 threads = []
 on_message_callback = functools.partial(on_message, args=(connection, threads))
-channel.basic_consume(on_message_callback, 'standard')
+channel.basic_consume(on_message_callback=on_message_callback, queue='standard')
 
 try:
     channel.start_consuming()
