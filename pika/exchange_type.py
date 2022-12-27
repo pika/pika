cd01048ed08c0d1824134251cad0891b03214b6d
liortct <liortct@gmail.com>
2022-12-27 21:34:56 +0100

    exchange type enum use StrEnum

--- pika/exchange_type.py
+++ pika/exchange_type.py
@@ -1,8 +1,18 @@
-from enum import Enum
+try:
+    from enum import StrEnum  # available from Python 3.11
 
 
-class ExchangeType(str, Enum):
-    direct = 'direct'
-    fanout = 'fanout'
-    headers = 'headers'
-    topic = 'topic'
+    class ExchangeType(StrEnum):
+        direct = 'direct'
+        fanout = 'fanout'
+        headers = 'headers'
+        topic = 'topic'
+except ImportError:
+    from enum import Enum
+
+
+    class ExchangeType(str, Enum):
+        direct = 'direct'
+        fanout = 'fanout'
+        headers = 'headers'
+        topic = 'topic'
