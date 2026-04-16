60f0fd14aadbd02a419b96d8d0d42ef5e6b8042c
Jacob Paluska-Yakel <91086572+jpaluska@users.noreply.github.com>
2026-04-15 17:58:48 -0700

    use DEBUG and LOG_FORMAT in basicConfig

--- examples/basic_publisher_threaded.py
+++ examples/basic_publisher_threaded.py
@@ -12,7 +12,7 @@ LOG_FORMAT = ("%(levelname) -10s %(asctime)s %(name) -30s %(funcName) "
               "-35s %(lineno) -5d: %(message)s")
 LOGGER = logging.getLogger(__name__)
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
 
 
 class ExampleThreadSafePublisher(object):
