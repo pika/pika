1d8cb2afc79d5de5911c3b7c9c0161480df0b4c8
Davide Di Mauro <dadodimauro@gmail.com>
2026-05-09 16:40:00 +0200

    fix: examples with rabbitmq 4.3 need durable queues

--- examples/long_running_publisher.py
+++ examples/long_running_publisher.py
@@ -19,7 +19,7 @@ class Publisher(threading.Thread):
         parameters = ConnectionParameters("localhost", credentials=credentials)
         self.connection = BlockingConnection(parameters)
         self.channel = self.connection.channel()
-        self.channel.queue_declare(queue=self.queue, auto_delete=True)
+        self.channel.queue_declare(queue=self.queue, durable=True, auto_delete=True)
 
     def run(self):
         while self.is_running:
