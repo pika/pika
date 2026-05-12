0eee354f89ada984b506009955d6580f8128dacb
Suchit Dhakate <suchit.dhakate@broadcom.com>
2026-05-12 16:36:01 +0530

    Fix type check error reported by _reactor.callLater in twisted_connection.py

--- pika/adapters/twisted_connection.py
+++ pika/adapters/twisted_connection.py
@@ -1148,9 +1148,9 @@ class _TwistedConnectionAdapter(pika.connection.Connection):
 
         """
         check_callback_arg(callback, 'callback')
-        return _TimerHandle(
-            self._reactor.callLater(delay, callback)
-        )  # type: ignore[arg-type] # pyright: ignore[reportAttributeAccessIssue]
+        reactor: Any = self._reactor
+        call_id = reactor.callLater(delay, callback)
+        return _TimerHandle(call_id)
 
     def _adapter_remove_timeout(self, timeout_id: Any) -> None:
         """Implement
