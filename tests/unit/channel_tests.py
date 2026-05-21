pika/pika

commit a611288d8ac34cb594f7fe0dd2eb9256c57eea9d

removal of pika.compat.dictkeys()

pika/channel.py
@@ -14,7 +14,7 @@ import pika.frame as frame
 import pika.exceptions as exceptions
 import pika.spec as spec
 import pika.validators as validators
-from pika.compat import as_bytes, dictkeys, is_integer
+from pika.compat import as_bytes, is_integer
 from pika.exchange_type import ExchangeType
 
 LOGGER = logging.getLogger(__name__)
@@ -540,7 +540,7 @@ class Channel:
         self._closing_reason = exceptions.ChannelClosedByClient(
             reply_code, reply_text)
 
-        for consumer_tag in dictkeys(self._consumers):
+        for consumer_tag in self._consumers.keys():
             if consumer_tag not in self._cancelled:
                 self.basic_cancel(consumer_tag=consumer_tag)
 
@@ -598,7 +598,7 @@ class Channel:
         :rtype: list
 
         """
-        return dictkeys(self._consumers)
+        return list(self._consumers.keys())
 
     def exchange_bind(self,
                       destination,

pika/adapters/blocking_connection.py
@@ -799,7 +799,7 @@ class BlockingConnection:
         LOGGER.info('Closing connection (%s): %s', reply_code, reply_text)
 
         # Close channels that remain opened
-        for impl_channel in compat.dictvalues(self._impl._channels):
+        for impl_channel in list(self._impl._channels.values()):
             channel = impl_channel._get_cookie()
             if channel.is_open:
                 try:
@@ -1324,7 +1324,7 @@ class BlockingChannel:
         :rtype: list
 
         """
-        return compat.dictkeys(self._consumer_infos)
+        return list(self._consumer_infos.keys())
 
     _ALWAYS_READY_WAITERS = ((lambda: True),)
 
@@ -1490,7 +1490,7 @@ class BlockingChannel:
                 self.cancel()
 
             # Cancel consumers created via basic_consume
-            for consumer_tag in compat.dictkeys(self._consumer_infos):
+            for consumer_tag in list(self._consumer_infos.keys()):
                 self.basic_cancel(consumer_tag)
 
     def _dispatch_events(self):

pika/adapters/select_connection.py
@@ -889,7 +889,7 @@ class _PollerBase(pika.compat.AbstractBase):  # pylint: disable=R0902
 
         self._processing_fd_event_map = fd_event_map
 
-        for fileno in pika.compat.dictkeys(fd_event_map):
+        for fileno in list(fd_event_map.keys()):
             if fileno not in fd_event_map:
                 # the fileno has been removed from the map under our feet.
                 continue

pika/compat.py
@@ -40,37 +40,6 @@ def time_now():
     return time.monotonic()
 
 
-def dictkeys(dct):
-    """
-    Returns a list of keys of dictionary
-    """
-
-    return list(dct.keys())
-
-
-def dictvalues(dct):
-    """
-    Returns a list of values of a dictionary
-    """
-    return list(dct.values())
-
-
-def dict_iteritems(dct):
-    """
-    Returns an iterator of items (key/value pairs) of a dictionary
-    """
-    return dct.items()
-
-
-def dict_itervalues(dct):
-    """
-    :param dict dct:
-    :returns: an iterator of the values of a dictionary
-    :rtype: iterator
-    """
-    return dct.values()
-
-
 def byte(*args):
     """
     Returns a single byte `bytes` for the given int argument (we

pika/connection.py
@@ -22,7 +22,6 @@ import pika.frame as frame
 import pika.heartbeat
 import pika.spec as spec
 import pika.validators as validators
-from pika.compat import (dictkeys, dict_itervalues, dict_iteritems)
 
 PRODUCT = "Pika Python Client Library"
 
@@ -760,7 +759,7 @@ class URLParameters(Parameters):
         # Handle query string values, validating and assigning them
         self._all_url_query_values = url_parse_qs(parts.query)
 
-        for name, value in dict_iteritems(self._all_url_query_values):
+        for name, value in self._all_url_query_values.items():
             try:
                 set_value = getattr(self, self._SETTER_PREFIX + name)
             except AttributeError:
@@ -1552,7 +1551,7 @@ class Connection(pika.compat.AbstractBase):
         """
         assert self.is_open, str(self)
 
-        for channel_number in dictkeys(self._channels):
+        for channel_number in list(self._channels.keys()):
             chan = self._channels[channel_number]
             if not (chan.is_closing or chan.is_closed):
                 chan.close(reply_code, reply_text)
@@ -1701,7 +1700,7 @@ class Connection(pika.compat.AbstractBase):
                 # should also be in CLOSING state. Deviation from this would
                 # prevent Connection from completing its closing procedure.
                 channels_not_in_closing_state = [
-                    chan for chan in dict_itervalues(self._channels)
+                    chan for chan in self._channels.values()
                     if not chan.is_closing
                 ]
                 if channels_not_in_closing_state:
@@ -2062,7 +2061,7 @@ class Connection(pika.compat.AbstractBase):
         self._set_connection_state(self.CONNECTION_CLOSED)
 
         # Inform our channel proxies, if any are still around
-        for channel in dictkeys(self._channels):
+        for channel in list(self._channels.keys()):
             if channel not in self._channels:
                 continue
             # pylint: disable=W0212

tests/unit/connection_parameters_tests.py
@@ -10,7 +10,6 @@ import warnings
 from urllib.parse import quote as url_quote, urlencode
 
 import pika
-from pika.compat import dict_iteritems
 from pika import channel, connection, credentials, spec
 
 # disable missing-docstring
@@ -100,8 +99,7 @@ class ParametersTestsBase(unittest.TestCase):
         :param pika.connection.Parameters params: Verify that the given params
             instance has all default property values
         """
-        for name, expected_value in dict_iteritems(
-                self.get_default_properties()):
+        for name, expected_value in self.get_default_properties().items():
             value = getattr(params, name)
             self.assertEqual(
                 value,

tests/unit/diagnostic_utils_test.py
@@ -6,7 +6,6 @@ import unittest
 
 import logging
 
-import pika.compat
 from pika import diagnostic_utils
 
 
@@ -47,7 +46,7 @@ class DiagnosticUtilsTest(unittest.TestCase):
         for i in range(len(expected_args)):
             self.assertIs(bucket[0][0][i], expected_args[i])
 
-        for key in pika.compat.dictkeys(expected_kwargs):
+        for key in expected_kwargs.keys():
             self.assertIs(bucket[0][1][key], expected_kwargs[key])
 
         # Now, repeat without any args/kwargs

commit 80d1d4c271a0b3b4ac45a007fee92c6abbac67de

Fix `Channel.close()` for channels with multiple consumers
Commit a611288d8ac34cb594f7fe0dd2eb9256c57eea9d introduced a bug
causing `Channel.close()` to fail for channels with more than one
consumer.  This commit fixes.

pika/channel.py
@@ -594,7 +594,7 @@ class Channel:
         self._closing_reason = exceptions.ChannelClosedByClient(
             reply_code, reply_text)
 
-        for consumer_tag in self._consumers.keys():
+        for consumer_tag in list(self._consumers.keys()):
             if consumer_tag not in self._cancelled:
                 self.basic_cancel(consumer_tag=consumer_tag)
 

tests/unit/channel_tests.py
@@ -670,6 +670,16 @@ class ChannelTests(unittest.TestCase):
             # every consumer before closing the channel
             basic_cancel.assert_called_once_with(consumer_tag='abc')
 
+    def test_close_basic_cancel_called_for_all_consumers(self):
+        self.obj._set_state(self.obj.OPEN)
+        self.obj._consumers['abc'] = None
+        self.obj._consumers['def'] = None
+        with mock.patch.object(self.obj, 'basic_cancel') as basic_cancel:
+            self.obj.close()
+            assert basic_cancel.call_count == 2
+            basic_cancel.assert_any_call(consumer_tag='abc')
+            basic_cancel.assert_any_call(consumer_tag='def')
+
     def test_confirm_delivery_with_bad_callback_raises_value_error(self):
         self.assertRaises(ValueError, self.obj.confirm_delivery, 'bad-callback')
 